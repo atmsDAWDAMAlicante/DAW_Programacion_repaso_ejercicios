5c56832cce2db5d1ade78accae4cb8452789f55f
atms1dawz <angmorsen@alu.edu.gva.es>
2023-12-11 19:03:03 +0100

    Programa completo

---
+++ Tema_04/Tema_04_Ejercicio_08/Prog_Tema_4_Ej_08/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Prog_Tema_4_Ej_08
+{
+    partial class Prog_Tema_4_Ej_08
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(27, 23);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(91, 26);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(27, 70);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(91, 26);
+            this.txtNum2.TabIndex = 2;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Location = new System.Drawing.Point(27, 119);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(91, 26);
+            this.txtNum3.TabIndex = 3;
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(27, 173);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(91, 34);
+            this.btnAccion.TabIndex = 4;
+            this.btnAccion.Text = "Acción";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(23, 242);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(25, 24);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "...";
+            // 
+            // Prog_Tema_4_Ej_08
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 333);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Prog_Tema_4_Ej_08";
+            this.Text = "Prog_Tema_4_Ej_08";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Tema_04/Tema_04_Ejercicio_08/Prog_Tema_4_Ej_08/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_4_Ej_08
+{
+    public partial class Prog_Tema_4_Ej_08 : Form
+    {
+        public Prog_Tema_4_Ej_08()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccion_Click(object sender, EventArgs e)
+        {
+            double num1 = 0, num2 = 0, num3 = 0;
+            ValidarCampos1(ref num1, ref num2, ref num3);
+        }
+
+
+        public void ValidarCampos1(ref double num1, ref double num2, ref double num3)
+        {
+            bool num1esnum = double.TryParse(txtNum1.Text, out num1);
+            bool num2esnum = double.TryParse(txtNum2.Text, out num2);
+            bool num3esnum = double.TryParse(txtNum3.Text, out num3);
+
+            if (num1esnum && num2esnum && num3esnum)
+            {
+                ValidarCampos2(ref num1, ref num2, ref num3);
+            }
+            else 
+            {
+                lblResultado.Text = "Introduce números";
+            }
+        }
+
+        public void ValidarCampos2(ref double num1, ref double num2, ref double num3)
+        {
+            if ((num1 >= 0 && num1 < 11) && (num2 >= 0 && num2 < 11) && (num3 >= 0 && num3 < 11))
+            {
+                CalcularLaMedia(ref num1, ref num2, ref num3);
+            }
+            else 
+            {
+                lblResultado.Text = "Introduce un\nvalor entre 0\ny 10";
+            }
+        }
+
+        public void CalcularLaMedia(ref double num1, ref double num2, ref double num3)
+        {
+            double media = (( num1 + num2 + num3 ) / 3 );
+            // lblResultado.Text = "La nota media\nes " + Math.Round(media, 2).ToString("#.##");
+            lblResultado.Text = $"La nota media\nes {media:F2}";
+        }
+    }
+
+}
