9c147b084227e140724bcc57fd73389b6e0bd5df
atms1dawz <angmorsen@alu.edu.gva.es>
2023-12-11 13:47:52 +0100

    Interfaz del programa

--- Tema_04/Tema_04_Ejercicio_06/Prog_Tema_4_Ej_06/Form1.Designer.cs
+++ Tema_04/Tema_04_Ejercicio_06/Prog_Tema_4_Ej_06/Form1.Designer.cs
@@ -28,20 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.lblTitulo1 = new System.Windows.Forms.Label();
+            this.lblTitulo2 = new System.Windows.Forms.Label();
+            this.lblResultadoDivision = new System.Windows.Forms.Label();
+            this.lblResto = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(27, 118);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(99, 29);
+            this.btnAccion.TabIndex = 2;
+            this.btnAccion.Text = "Acción";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo1
+            // 
+            this.lblTitulo1.AutoSize = true;
+            this.lblTitulo1.Location = new System.Drawing.Point(31, 179);
+            this.lblTitulo1.Name = "lblTitulo1";
+            this.lblTitulo1.Size = new System.Drawing.Size(135, 40);
+            this.lblTitulo1.TabIndex = 3;
+            this.lblTitulo1.Text = "El resultado de la\ndivisión entera es:";
+            // 
+            // lblTitulo2
+            // 
+            this.lblTitulo2.AutoSize = true;
+            this.lblTitulo2.Location = new System.Drawing.Point(31, 270);
+            this.lblTitulo2.Name = "lblTitulo2";
+            this.lblTitulo2.Size = new System.Drawing.Size(88, 20);
+            this.lblTitulo2.TabIndex = 4;
+            this.lblTitulo2.Text = "El resto es:";
+            // 
+            // lblResultadoDivision
+            // 
+            this.lblResultadoDivision.AutoSize = true;
+            this.lblResultadoDivision.Location = new System.Drawing.Point(31, 230);
+            this.lblResultadoDivision.Name = "lblResultadoDivision";
+            this.lblResultadoDivision.Size = new System.Drawing.Size(25, 24);
+            this.lblResultadoDivision.TabIndex = 5;
+            this.lblResultadoDivision.Text = "...";
+            // 
+            // lblResto
+            // 
+            this.lblResto.AutoSize = true;
+            this.lblResto.Location = new System.Drawing.Point(31, 311);
+            this.lblResto.Name = "lblResto";
+            this.lblResto.Size = new System.Drawing.Size(25, 24);
+            this.lblResto.TabIndex = 6;
+            this.lblResto.Text = "...";
             // 
             // Prog_Tema_4_Ej_06
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(193, 352);
+            this.Controls.Add(this.lblResto);
+            this.Controls.Add(this.lblResultadoDivision);
+            this.Controls.Add(this.lblTitulo2);
+            this.Controls.Add(this.lblTitulo1);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Prog_Tema_4_Ej_06";
             this.Text = "Prog_Tema_4_Ej_06";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Label lblTitulo1;
+        private System.Windows.Forms.Label lblTitulo2;
+        private System.Windows.Forms.Label lblResultadoDivision;
+        private System.Windows.Forms.Label lblResto;
     }
 }
 
