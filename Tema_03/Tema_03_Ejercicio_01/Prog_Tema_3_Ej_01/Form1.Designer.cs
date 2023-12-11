85d2ce14534fa97a8795d2cb027f7886bed16fc6
atms1dawz <angmorsen@alu.edu.gva.es>
2023-12-11 19:59:30 +0100

    Programa completo

---
+++ Tema_03/Tema_03_Ejercicio_01/Prog_Tema_3_Ej_01/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Prog_Tema_3_Ej_01
+{
+    partial class Prog_Tema_3_Ej_01
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(36, 23);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(100, 26);
+            this.txtDato.TabIndex = 0;
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(36, 78);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(100, 36);
+            this.btnAccion.TabIndex = 1;
+            this.btnAccion.Text = "Acción";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(32, 139);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(25, 24);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "...";
+            // 
+            // Prog_Tema_3_Ej_01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 230);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.txtDato);
+            this.Name = "Prog_Tema_3_Ej_01";
+            this.Text = "Prog_Tema_3_Ej_01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Tema_03/Tema_03_Ejercicio_01/Prog_Tema_3_Ej_01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_3_Ej_01
+{
+    public partial class Prog_Tema_3_Ej_01 : Form
+    {
+        public Prog_Tema_3_Ej_01()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccion_Click(object sender, EventArgs e)
+        {
+            string dato = txtDato.Text;
+            if (dato == "2")
+            {
+                lblResultado.Text = $"El número\nintroducido\nes el {dato}";
+            } else
+            {
+                lblResultado.Text = "El número\nintroducido\nNO es el 2";
+            }
+        }
+    }
+}
