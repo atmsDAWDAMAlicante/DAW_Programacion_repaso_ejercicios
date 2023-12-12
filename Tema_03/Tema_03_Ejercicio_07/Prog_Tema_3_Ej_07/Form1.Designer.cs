d1e97c5b1b9c4ca6cafbd3d3c1a0d4e5fb95d699
atms1dawz <angmorsen@alu.edu.gva.es>
2023-12-12 19:38:09 +0100

    Interfaz del programa

---
+++ Tema_03/Tema_03_Ejercicio_07/Prog_Tema_3_Ej_07/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Prog_Tema_3_Ej_07
+{
+    partial class Prog_Tema_3_Ej_07
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 12);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 26);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(12, 56);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(100, 40);
+            this.btnAccion.TabIndex = 1;
+            this.btnAccion.Text = "Acción";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 124);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(25, 24);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "...";
+            // 
+            // Prog_Tema_3_Ej_07
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 204);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Prog_Tema_3_Ej_07";
+            this.Text = "Prog_Tema_3_Ej_07";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
