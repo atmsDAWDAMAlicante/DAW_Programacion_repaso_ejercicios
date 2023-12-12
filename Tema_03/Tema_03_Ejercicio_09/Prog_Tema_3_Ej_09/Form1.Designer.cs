ed9def63411a7842107d3af0d00fcafd4ec6d78a
atms1dawz <angmorsen@alu.edu.gva.es>
2023-12-12 20:36:01 +0100

    Programa completo

---
+++ Tema_03/Tema_03_Ejercicio_09/Prog_Tema_3_Ej_09/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Prog_Tema_3_Ej_09
+{
+    partial class Prog_Tema_3_Ej_09
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnSuCambio = new System.Windows.Forms.Button();
+            this.grpSuCambio = new System.Windows.Forms.GroupBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.grpSuCambio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(12, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(256, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Introduce una cantidad en pesetas";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(15, 59);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(138, 26);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // btnSuCambio
+            // 
+            this.btnSuCambio.Location = new System.Drawing.Point(159, 55);
+            this.btnSuCambio.Name = "btnSuCambio";
+            this.btnSuCambio.Size = new System.Drawing.Size(109, 37);
+            this.btnSuCambio.TabIndex = 2;
+            this.btnSuCambio.Text = "Su cambio";
+            this.btnSuCambio.UseVisualStyleBackColor = true;
+            this.btnSuCambio.Click += new System.EventHandler(this.btnSuCambio_Click);
+            // 
+            // grpSuCambio
+            // 
+            this.grpSuCambio.Controls.Add(this.lblResultado);
+            this.grpSuCambio.Location = new System.Drawing.Point(16, 98);
+            this.grpSuCambio.Name = "grpSuCambio";
+            this.grpSuCambio.Size = new System.Drawing.Size(252, 332);
+            this.grpSuCambio.TabIndex = 0;
+            this.grpSuCambio.TabStop = false;
+            this.grpSuCambio.Text = "Su cambio:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(6, 35);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 24);
+            this.lblResultado.TabIndex = 0;
+            // 
+            // Prog_Tema_3_Ej_09
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 450);
+            this.Controls.Add(this.grpSuCambio);
+            this.Controls.Add(this.btnSuCambio);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Prog_Tema_3_Ej_09";
+            this.Text = "Prog_Tema_3_Ej_09";
+            this.grpSuCambio.ResumeLayout(false);
+            this.grpSuCambio.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnSuCambio;
+        private System.Windows.Forms.GroupBox grpSuCambio;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Tema_03/Tema_03_Ejercicio_09/Prog_Tema_3_Ej_09/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_3_Ej_09
+{
+    public partial class Prog_Tema_3_Ej_09 : Form
+    {
+        public Prog_Tema_3_Ej_09()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuCambio_Click(object sender, EventArgs e)
+        {
+            int unidades = 0;
+            string tipoMoneda = "billete/s";
+            lblResultado.Text = "";
+            bool numesnum = int.TryParse(txtCantidad.Text, out int cantidad);
+            if (numesnum)
+            {
+                if (cantidad > 0)
+                {
+                    // Billetes de 10.000 pesetas
+                    while (cantidad >= 10000)
+                    {
+                         unidades++;
+                         cantidad -= 10000;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 10.000 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Billetes de 5.000 pesetas
+                    while (cantidad >= 5000)
+                    {
+                        unidades++;
+                        cantidad -= 5000;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 5.000 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Billetes de 2.000 pesetas
+                    while (cantidad >= 2000)
+                    {
+                        unidades++;
+                        cantidad -= 2000;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 2.000 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Billetes de 1.000 pesetas
+                    while (cantidad >= 1000)
+                    {
+                        unidades++;
+                        cantidad -= 1000;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 1.000 ptas\n";
+                    }
+
+                    unidades = 0;
+                    tipoMoneda = "moneda/s";
+
+                    // Monedas de 500 pesetas
+                    while (cantidad >= 500)
+                    {
+                        unidades++;
+                        cantidad -= 500;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 500 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 200 pesetas
+                    while (cantidad >= 200)
+                    {
+                        unidades++;
+                        cantidad -= 200;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 200 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 100 pesetas
+                    while (cantidad >= 100)
+                    {
+                        unidades++;
+                        cantidad -= 100;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 100 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 50 pesetas
+                    while (cantidad >= 50)
+                    {
+                        unidades++;
+                        cantidad -= 50;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 50 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 25 pesetas
+                    while (cantidad >= 25)
+                    {
+                        unidades++;
+                        cantidad -= 25;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 25 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 10 pesetas
+                    while (cantidad >= 10)
+                    {
+                        unidades++;
+                        cantidad -= 10;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 10 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 5 pesetas
+                    while (cantidad >= 5)
+                    {
+                        unidades++;
+                        cantidad -= 5;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 5 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 2 pesetas
+                    while (cantidad >= 2)
+                    {
+                        unidades++;
+                        cantidad -= 2;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 2 ptas\n";
+                    }
+
+                    unidades = 0;
+
+                    // Monedas de 1 pesetas
+                    while (cantidad >= 1)
+                    {
+                        unidades++;
+                        cantidad -= 1;
+                    }
+
+                    if (unidades > 0)
+                    {
+                        lblResultado.Text += $"{unidades} {tipoMoneda} de 1 ptas\n";
+                    }
+
+
+
+                }
+                else
+                {
+                    lblResultado.Text = "Introduzca una cantidad positiva";
+                }
+
+
+
+
+            }
+
+
+            else
+            {
+                lblResultado.Text = "Introduzca números";
+            }
+        }
+    }
+}
