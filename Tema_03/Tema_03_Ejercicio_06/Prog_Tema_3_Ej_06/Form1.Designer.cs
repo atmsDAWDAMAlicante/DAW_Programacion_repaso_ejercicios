23010bb85b9f210d0d9bb2c5d5b5c0ef43ef0d93
atms1dawz <angmorsen@alu.edu.gva.es>
2023-12-12 19:31:08 +0100

    Programa completo

---
+++ Tema_03/Tema_03_Ejercicio_06/Prog_Tema_3_Ej_06/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Prog_Tema_3_Ej_06
+{
+    partial class Prog_Tema_3_Ej_06
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNota
+            // 
+            this.txtNota.Location = new System.Drawing.Point(12, 12);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(100, 26);
+            this.txtNota.TabIndex = 0;
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(12, 56);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(100, 33);
+            this.btnAccion.TabIndex = 1;
+            this.btnAccion.Text = "Acción";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 106);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(25, 24);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "...";
+            // 
+            // Prog_Tema_3_Ej_06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 184);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.txtNota);
+            this.Name = "Prog_Tema_3_Ej_06";
+            this.Text = "Prog_Tema_3_Ej_06";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Tema_03/Tema_03_Ejercicio_06/Prog_Tema_3_Ej_06/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_3_Ej_06
+{
+    public partial class Prog_Tema_3_Ej_06 : Form
+    {
+        public Prog_Tema_3_Ej_06()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccion_Click(object sender, EventArgs e)
+        {
+            bool notaesnum = double.TryParse(txtNota.Text, out double nota);
+            if (notaesnum)
+            {
+                if (nota >= 0 && nota < 3)
+                {
+                    lblResultado.Text = "Muy deficiente";
+                }
+                else if (nota >= 3 && nota < 5)
+                {
+                    lblResultado.Text = "Insuficiente";
+                }
+                else if (nota >= 5 && nota < 6)
+                {
+                    lblResultado.Text = "Suficiente";
+                }
+                else if (nota >= 6 && nota < 7)
+                {
+                    lblResultado.Text = "Bien";
+                }
+                else if (nota >= 7 && nota < 9)
+                {
+                    lblResultado.Text = "Notable";
+                }
+                else if (nota >= 9 && nota <= 10)
+                {
+                    lblResultado.Text = "Sobresaliente";
+                }
+                else
+                {
+                    lblResultado.Text = "Introduce un\nnúmero entre\n0 y 10";
+                }
+
+
+            } else
+            {
+                lblResultado.Text = "Inroduce números";
+            }
+        }
+    }
+}
