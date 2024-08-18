66ff3c50e54ce663abe0e66dcbb16a0088eb3821
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-18 11:57:24 +0200

    Programa completo

--- Tema_02/Tema_02_Ejercicio_06/Prog_Tema_2_Ej_06/Form1.Designer.cs
+++ Tema_02/Tema_02_Ejercicio_06/Prog_Tema_2_Ej_06/Form1.Designer.cs
@@ -28,20 +28,83 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblNumero = new System.Windows.Forms.Label();
+			this.lblDivisor = new System.Windows.Forms.Label();
+			this.txtNumero = new System.Windows.Forms.TextBox();
+			this.txtDivisor = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.LimeGreen;
+			this.btnAccion.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.ForeColor = System.Drawing.Color.Yellow;
+			this.btnAccion.Location = new System.Drawing.Point(265, 34);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(104, 46);
+			this.btnAccion.TabIndex = 3;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// lblNumero
+			// 
+			this.lblNumero.AutoSize = true;
+			this.lblNumero.Location = new System.Drawing.Point(51, 24);
+			this.lblNumero.Name = "lblNumero";
+			this.lblNumero.Size = new System.Drawing.Size(83, 24);
+			this.lblNumero.TabIndex = 0;
+			this.lblNumero.Text = "Número:";
+			// 
+			// lblDivisor
+			// 
+			this.lblDivisor.AutoSize = true;
+			this.lblDivisor.Location = new System.Drawing.Point(51, 77);
+			this.lblDivisor.Name = "lblDivisor";
+			this.lblDivisor.Size = new System.Drawing.Size(72, 24);
+			this.lblDivisor.TabIndex = 0;
+			this.lblDivisor.Text = "Divisor:";
+			// 
+			// txtNumero
+			// 
+			this.txtNumero.Location = new System.Drawing.Point(133, 18);
+			this.txtNumero.Name = "txtNumero";
+			this.txtNumero.Size = new System.Drawing.Size(100, 26);
+			this.txtNumero.TabIndex = 1;
+			// 
+			// txtDivisor
+			// 
+			this.txtDivisor.Location = new System.Drawing.Point(133, 71);
+			this.txtDivisor.Name = "txtDivisor";
+			this.txtDivisor.Size = new System.Drawing.Size(100, 26);
+			this.txtDivisor.TabIndex = 2;
 			// 
 			// Prog_Tema_2_Ej_06
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.PaleGreen;
+			this.ClientSize = new System.Drawing.Size(453, 127);
+			this.Controls.Add(this.txtDivisor);
+			this.Controls.Add(this.txtNumero);
+			this.Controls.Add(this.lblDivisor);
+			this.Controls.Add(this.lblNumero);
+			this.Controls.Add(this.btnAccion);
 			this.Name = "Prog_Tema_2_Ej_06";
 			this.Text = "Prog_Tema_2_Ej_06";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnAccion;
+		private System.Windows.Forms.Label lblNumero;
+		private System.Windows.Forms.Label lblDivisor;
+		private System.Windows.Forms.TextBox txtNumero;
+		private System.Windows.Forms.TextBox txtDivisor;
 		}
 	}
 
--- Tema_02/Tema_02_Ejercicio_06/Prog_Tema_2_Ej_06/Form1.cs
+++ Tema_02/Tema_02_Ejercicio_06/Prog_Tema_2_Ej_06/Form1.cs
@@ -16,5 +16,32 @@ namespace Prog_Tema_2_Ej_06
 			{
 			InitializeComponent();
 			}
+
+		private void btnAccion_Click(object sender, EventArgs e)
+			{
+			try 
+				{ 
+				// Declaración, inicialización y conversión en enteros de las variables que recogen los valores de los TextBox
+				int numero = int.Parse(txtNumero.Text);
+				int divisor = int.Parse(txtDivisor.Text);
+
+				// Declaración y operaciones de las dos variables que recogen los resultados de éstas
+				int division = numero / divisor;
+				int resto = numero % divisor;
+
+				// Muestra de los resultados mediante un MessageBox
+				MessageBox.Show("Resultados:\nDivisión = " + division.ToString() + "\nResto = " + resto.ToString());
+				}
+
+			catch (FormatException) // captura del error de la introducción de caracteres no numéricos
+				{
+				MessageBox.Show("Introduzca números.");
+				}
+			catch (DivideByZeroException) // captura del error de la división por cero
+				{
+				MessageBox.Show("Error: división por cero");
+				}
+
+			}
 		}
 	}
