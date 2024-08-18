bf1c0cf9f725db25a5bcb86a0baaf4a7d2aef6c8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-18 10:17:10 +0200

    Programa completo

--- Tema_01/Tema_01_Ejercicio_01/Prog_Tema_1_Ej_01/Form1.Designer.cs
+++ Tema_01/Tema_01_Ejercicio_01/Prog_Tema_1_Ej_01/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Prog_Tema_1_Ej_01));
 			this.btnBoton1 = new System.Windows.Forms.Button();
 			this.btnBoton2 = new System.Windows.Forms.Button();
 			this.lblTitulo = new System.Windows.Forms.Label();
@@ -69,6 +70,8 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.LightSkyBlue;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.ClientSize = new System.Drawing.Size(546, 235);
 			this.Controls.Add(this.lblTitulo);
 			this.Controls.Add(this.btnBoton2);
