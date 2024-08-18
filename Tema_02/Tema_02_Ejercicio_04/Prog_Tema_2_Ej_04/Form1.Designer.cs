5b703b66cd4438dbcfe3634c405cd8b751ee8c78
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-18 10:59:31 +0200

    Programa completo

--- Tema_02/Tema_02_Ejercicio_04/Prog_Tema_2_Ej_04/Form1.Designer.cs
+++ Tema_02/Tema_02_Ejercicio_04/Prog_Tema_2_Ej_04/Form1.Designer.cs
@@ -28,20 +28,55 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.IndianRed;
+			this.btnAccion.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.ForeColor = System.Drawing.Color.White;
+			this.btnAccion.Location = new System.Drawing.Point(29, 13);
+			this.btnAccion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(143, 76);
+			this.btnAccion.TabIndex = 0;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.ForeColor = System.Drawing.Color.Black;
+			this.lblResultado.Location = new System.Drawing.Point(25, 114);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(99, 24);
+			this.lblResultado.TabIndex = 1;
+			this.lblResultado.Text = "Resultado:\n";
 			// 
 			// Prog_Tema_2_Ej_04
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.PeachPuff;
+			this.ClientSize = new System.Drawing.Size(382, 269);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnAccion);
+			this.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.Name = "Prog_Tema_2_Ej_04";
 			this.Text = "Prog_Tema_2_Ej_04";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnAccion;
+		private System.Windows.Forms.Label lblResultado;
 		}
 	}
 
--- Tema_02/Tema_02_Ejercicio_04/Prog_Tema_2_Ej_04/Form1.cs
+++ Tema_02/Tema_02_Ejercicio_04/Prog_Tema_2_Ej_04/Form1.cs
@@ -16,5 +16,27 @@ namespace Prog_Tema_2_Ej_04
 			{
 			InitializeComponent();
 			}
+
+		private void btnAccion_Click(object sender, EventArgs e)
+			{
+			lblResultado.Text = "Resultado:\n";
+
+			// Declaración de las dos variables enteros
+			int num1, num2;
+			lblResultado.Text += "- Declaración de dos varialbes enteros num1 y num2\n";
+
+			// Inicialización de las dos variables
+			num1 = 2;
+			num2 = 3;
+			lblResultado.Text += "- Inicialización de las dos variables num1 = 2 y num2 = 3\n";
+
+			//Declaración de la variable entera resultado que recoge la suma de num1 + num2
+			int resultado = num1 + num2;
+			lblResultado.Text += "- Declaración de la variable entera 'resultado' que\nrecoge la suma de num1 + num2\n";
+
+			// Impresión del resultado en un MessageBox
+			MessageBox.Show(resultado.ToString());
+			lblResultado.Text += "- Impresión del resultado de la suma = " + resultado.ToString();
+			}
 		}
 	}
