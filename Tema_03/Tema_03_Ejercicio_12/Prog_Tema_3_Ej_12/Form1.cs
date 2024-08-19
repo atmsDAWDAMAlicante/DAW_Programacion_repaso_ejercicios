205bb67048f51704a5a99a476c5442a203aae51e
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-19 16:37:41 +0200

    Commit inicial

---
+++ Tema_03/Tema_03_Ejercicio_12/Prog_Tema_3_Ej_12/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_3_Ej_12
+	{
+	public partial class Prog_Tema_3_Ej_12 : Form
+		{
+		public Prog_Tema_3_Ej_12()
+			{
+			InitializeComponent();
+			}
+		}
+	}
---
+++ Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Prog_Tema_3_Ej_25
+	{
+	partial class Prog_Tema_3_Ej_25
+		{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+			{
+			if (disposing && (components != null))
+				{
+				components.Dispose();
+				}
+			base.Dispose(disposing);
+			}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+			{
+			this.SuspendLayout();
+			// 
+			// Prog_Tema_3_Ej_25
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "Prog_Tema_3_Ej_25";
+			this.Text = "Prog_Tema_3_Ej_25";
+			this.ResumeLayout(false);
+
+			}
+
+		#endregion
+		}
+	}
+
