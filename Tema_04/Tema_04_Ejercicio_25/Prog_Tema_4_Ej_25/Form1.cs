95d848e4f7e0d3cff71d323de15b96c4fa8368d8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-19 19:17:30 +0200

    Commit inicial

---
+++ Tema_04/Tema_04_Ejercicio_25/Prog_Tema_4_Ej_25/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_4_Ej_25
+	{
+	public partial class Prog_Tema_4_Ej_25 : Form
+		{
+		public Prog_Tema_4_Ej_25()
+			{
+			InitializeComponent();
+			}
+		}
+	}
