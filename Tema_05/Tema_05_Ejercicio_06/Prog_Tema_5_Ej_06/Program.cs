2334775104a8744b7fe2ef3afd33eff7441a6882
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-20 19:44:00 +0200

    Commit inicial

---
+++ Tema_05/Tema_05_Ejercicio_06/Prog_Tema_5_Ej_06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_5_Ej_06
+	{
+	internal static class Program
+		{
+		/// <summary>
+		/// Punto de entrada principal para la aplicación.
+		/// </summary>
+		[STAThread]
+		static void Main()
+			{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Prog_Tema_5_Ej_06());
+			}
+		}
+	}
