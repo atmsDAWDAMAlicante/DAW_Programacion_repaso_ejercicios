ce63574f9e4bf4c6cf71c675d3306f9dc5ed6570
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-24 19:58:43 +0200

    Diseño del Form (colores, fondo, labels)

--- Tema_03/Tema_03_Ejercicio_08/Prog_Tema_3_Ej_08/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_08/Prog_Tema_3_Ej_08/Form1.Designer.cs
@@ -28,61 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtIntroducir = new System.Windows.Forms.TextBox();
-            this.btnAccion = new System.Windows.Forms.Button();
-            this.grpTexto = new System.Windows.Forms.GroupBox();
-            this.lblResultado = new System.Windows.Forms.Label();
-            this.grpTexto.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // txtIntroducir
-            // 
-            this.txtIntroducir.Location = new System.Drawing.Point(26, 22);
-            this.txtIntroducir.Name = "txtIntroducir";
-            this.txtIntroducir.Size = new System.Drawing.Size(278, 26);
-            this.txtIntroducir.TabIndex = 1;
-            // 
-            // btnAccion
-            // 
-            this.btnAccion.Location = new System.Drawing.Point(26, 66);
-            this.btnAccion.Name = "btnAccion";
-            this.btnAccion.Size = new System.Drawing.Size(119, 35);
-            this.btnAccion.TabIndex = 2;
-            this.btnAccion.Text = "Acción";
-            this.btnAccion.UseVisualStyleBackColor = true;
-            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
-            // 
-            // grpTexto
-            // 
-            this.grpTexto.Controls.Add(this.lblResultado);
-            this.grpTexto.Location = new System.Drawing.Point(26, 125);
-            this.grpTexto.Name = "grpTexto";
-            this.grpTexto.Size = new System.Drawing.Size(278, 302);
-            this.grpTexto.TabIndex = 0;
-            this.grpTexto.TabStop = false;
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(16, 22);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(0, 24);
-            this.lblResultado.TabIndex = 3;
-            // 
-            // Prog_Tema_3_Ej_08
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 450);
-            this.Controls.Add(this.grpTexto);
-            this.Controls.Add(this.btnAccion);
-            this.Controls.Add(this.txtIntroducir);
-            this.Name = "Prog_Tema_3_Ej_08";
-            this.Text = "Prog_Tema_3_Ej_08";
-            this.grpTexto.ResumeLayout(false);
-            this.grpTexto.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtIntroducir = new System.Windows.Forms.TextBox();
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.grpTexto = new System.Windows.Forms.GroupBox();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.grpTexto.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtIntroducir
+			// 
+			this.txtIntroducir.Location = new System.Drawing.Point(26, 88);
+			this.txtIntroducir.Name = "txtIntroducir";
+			this.txtIntroducir.Size = new System.Drawing.Size(278, 26);
+			this.txtIntroducir.TabIndex = 1;
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.ForeColor = System.Drawing.Color.Gold;
+			this.btnAccion.Location = new System.Drawing.Point(26, 126);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(142, 50);
+			this.btnAccion.TabIndex = 2;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// grpTexto
+			// 
+			this.grpTexto.Controls.Add(this.lblResultado);
+			this.grpTexto.ForeColor = System.Drawing.Color.Gold;
+			this.grpTexto.Location = new System.Drawing.Point(26, 182);
+			this.grpTexto.Name = "grpTexto";
+			this.grpTexto.Size = new System.Drawing.Size(278, 337);
+			this.grpTexto.TabIndex = 0;
+			this.grpTexto.TabStop = false;
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Location = new System.Drawing.Point(16, 22);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(120, 24);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "(su mensaje)";
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.Gold;
+			this.lblTitulo.Location = new System.Drawing.Point(26, 9);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(314, 39);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "Impresión de texto";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.ForeColor = System.Drawing.Color.Gold;
+			this.lblSubtitulo.Location = new System.Drawing.Point(26, 57);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(182, 25);
+			this.lblSubtitulo.TabIndex = 0;
+			this.lblSubtitulo.Text = "Introduzca su texto:";
+			// 
+			// Prog_Tema_3_Ej_08
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DarkCyan;
+			this.ClientSize = new System.Drawing.Size(344, 538);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.grpTexto);
+			this.Controls.Add(this.btnAccion);
+			this.Controls.Add(this.txtIntroducir);
+			this.Name = "Prog_Tema_3_Ej_08";
+			this.Text = "Prog_Tema_3_Ej_08";
+			this.grpTexto.ResumeLayout(false);
+			this.grpTexto.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -92,6 +124,8 @@
         private System.Windows.Forms.Button btnAccion;
         private System.Windows.Forms.GroupBox grpTexto;
         private System.Windows.Forms.Label lblResultado;
-    }
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblSubtitulo;
+		}
 }
 
