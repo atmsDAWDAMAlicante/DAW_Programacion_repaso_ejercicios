9176bee3cf893e6e9b3435a7a70a6cd5d5fcb957
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-24 12:03:09 +0200

    Programa completo

--- Tema_03/Tema_03_Ejercicio_02/Prog_Tema_3_Ej_02/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_02/Prog_Tema_3_Ej_02/Form1.Designer.cs
@@ -28,58 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNumero1 = new System.Windows.Forms.TextBox();
-            this.txtNumero2 = new System.Windows.Forms.TextBox();
-            this.btnAccion = new System.Windows.Forms.Button();
-            this.lblResultado = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // txtNumero1
-            // 
-            this.txtNumero1.Location = new System.Drawing.Point(37, 20);
-            this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(100, 26);
-            this.txtNumero1.TabIndex = 0;
-            // 
-            // txtNumero2
-            // 
-            this.txtNumero2.Location = new System.Drawing.Point(37, 70);
-            this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(100, 26);
-            this.txtNumero2.TabIndex = 1;
-            // 
-            // btnAccion
-            // 
-            this.btnAccion.Location = new System.Drawing.Point(37, 129);
-            this.btnAccion.Name = "btnAccion";
-            this.btnAccion.Size = new System.Drawing.Size(100, 32);
-            this.btnAccion.TabIndex = 2;
-            this.btnAccion.Text = "Acción";
-            this.btnAccion.UseVisualStyleBackColor = true;
-            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(33, 193);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(25, 24);
-            this.lblResultado.TabIndex = 3;
-            this.lblResultado.Text = "...";
-            // 
-            // Prog_Tema_3_Ej_02
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(176, 305);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.btnAccion);
-            this.Controls.Add(this.txtNumero2);
-            this.Controls.Add(this.txtNumero1);
-            this.Name = "Prog_Tema_3_Ej_02";
-            this.Text = "Prog_Tema_3_Ej_02";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtNumero1 = new System.Windows.Forms.TextBox();
+			this.txtNumero2 = new System.Windows.Forms.TextBox();
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblNum1 = new System.Windows.Forms.Label();
+			this.lblNum2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtNumero1
+			// 
+			this.txtNumero1.Location = new System.Drawing.Point(234, 95);
+			this.txtNumero1.Name = "txtNumero1";
+			this.txtNumero1.Size = new System.Drawing.Size(100, 26);
+			this.txtNumero1.TabIndex = 0;
+			// 
+			// txtNumero2
+			// 
+			this.txtNumero2.Location = new System.Drawing.Point(234, 140);
+			this.txtNumero2.Name = "txtNumero2";
+			this.txtNumero2.Size = new System.Drawing.Size(100, 26);
+			this.txtNumero2.TabIndex = 1;
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.SpringGreen;
+			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.Location = new System.Drawing.Point(145, 189);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(144, 44);
+			this.btnAccion.TabIndex = 2;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.ForeColor = System.Drawing.Color.White;
+			this.lblResultado.Location = new System.Drawing.Point(58, 250);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(108, 25);
+			this.lblResultado.TabIndex = 3;
+			this.lblResultado.Text = "Resultado";
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.White;
+			this.lblTitulo.Location = new System.Drawing.Point(53, 9);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(310, 52);
+			this.lblTitulo.TabIndex = 4;
+			this.lblTitulo.Text = "¿Son iguales?";
+			// 
+			// lblNum1
+			// 
+			this.lblNum1.AutoSize = true;
+			this.lblNum1.ForeColor = System.Drawing.Color.White;
+			this.lblNum1.Location = new System.Drawing.Point(58, 98);
+			this.lblNum1.Name = "lblNum1";
+			this.lblNum1.Size = new System.Drawing.Size(161, 20);
+			this.lblNum1.TabIndex = 5;
+			this.lblNum1.Text = "Introduce un número:";
+			// 
+			// lblNum2
+			// 
+			this.lblNum2.AutoSize = true;
+			this.lblNum2.ForeColor = System.Drawing.Color.White;
+			this.lblNum2.Location = new System.Drawing.Point(58, 143);
+			this.lblNum2.Name = "lblNum2";
+			this.lblNum2.Size = new System.Drawing.Size(171, 20);
+			this.lblNum2.TabIndex = 6;
+			this.lblNum2.Text = "Introduce otro número:";
+			// 
+			// Prog_Tema_3_Ej_02
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.ClientSize = new System.Drawing.Size(472, 305);
+			this.Controls.Add(this.lblNum2);
+			this.Controls.Add(this.lblNum1);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnAccion);
+			this.Controls.Add(this.txtNumero2);
+			this.Controls.Add(this.txtNumero1);
+			this.Name = "Prog_Tema_3_Ej_02";
+			this.Text = "Prog_Tema_3_Ej_02";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -89,6 +131,9 @@
         private System.Windows.Forms.TextBox txtNumero2;
         private System.Windows.Forms.Button btnAccion;
         private System.Windows.Forms.Label lblResultado;
-    }
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblNum1;
+		private System.Windows.Forms.Label lblNum2;
+		}
 }
 
--- Tema_03/Tema_03_Ejercicio_02/Prog_Tema_3_Ej_02/Form1.cs
+++ Tema_03/Tema_03_Ejercicio_02/Prog_Tema_3_Ej_02/Form1.cs
@@ -19,18 +19,25 @@ namespace Prog_Tema_3_Ej_02
 
         private void btnAccion_Click(object sender, EventArgs e)
         {
-            bool num1esnum = int.TryParse(txtNumero1.Text, out int num1);
+			// int.TryParse evalúan para cada TextBox si lo introducido es un número
+            // Si lo es lo convierte en un entero
+            // Las variables num1esnum y num2esnum recogen un valor booleano en función de si tiene éxito la conversión a entero
+			// No se utiliza try-catch
+			bool num1esnum = int.TryParse(txtNumero1.Text, out int num1);
             bool num2esnum = int.TryParse(txtNumero2.Text, out int num2);
 
-            if (num1esnum && num2esnum)
+			// Bloque condicional if que evalúa si ha tenido éxito la conversión a entero de lo introducido en los TextBox
+			if (num1esnum && num2esnum)
             {
-                if (num1 == num2)
+				// Bloque condicional if para evaluar si los números son iguales
+				// Los resultados se imprimen utilizando plantillas (templates) literales
+				if (num1 == num2)
                 {
-                    lblResultado.Text = $"{num1} y {num2}\nson iguales";
+                    lblResultado.Text = $"{num1} y {num2}: ¡Dos iguales para hoy!";
                 } 
                 else
                 {
-                    lblResultado.Text = $"{num1} y {num2}\nNO son iguales";
+                    lblResultado.Text = $"{num1} y {num2} NO son iguales";
                 }
             } else
             {
