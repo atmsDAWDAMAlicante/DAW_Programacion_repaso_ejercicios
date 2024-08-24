50e312157aa37599b7d9c7b78ecc055d1b59711e
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-24 17:49:09 +0200

    Programa completo

--- Tema_03/Tema_03_Ejercicio_05/Prog_Tema_3_Ej_05/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_05/Prog_Tema_3_Ej_05/Form1.Designer.cs
@@ -29,67 +29,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNumero1 = new System.Windows.Forms.TextBox();
-            this.txtNumero2 = new System.Windows.Forms.TextBox();
-            this.btnAccion = new System.Windows.Forms.Button();
-            this.lblResultado = new System.Windows.Forms.Label();
-            this.txtNumero3 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // txtNumero1
-            // 
-            this.txtNumero1.Location = new System.Drawing.Point(12, 12);
-            this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(100, 26);
-            this.txtNumero1.TabIndex = 1;
-            // 
-            // txtNumero2
-            // 
-            this.txtNumero2.Location = new System.Drawing.Point(12, 63);
-            this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(100, 26);
-            this.txtNumero2.TabIndex = 2;
-            // 
-            // btnAccion
-            // 
-            this.btnAccion.Location = new System.Drawing.Point(12, 176);
-            this.btnAccion.Name = "btnAccion";
-            this.btnAccion.Size = new System.Drawing.Size(100, 41);
-            this.btnAccion.TabIndex = 3;
-            this.btnAccion.Text = "Acción";
-            this.btnAccion.UseVisualStyleBackColor = true;
-            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(8, 252);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(21, 20);
-            this.lblResultado.TabIndex = 0;
-            this.lblResultado.Text = "...";
-            // 
-            // txtNumero3
-            // 
-            this.txtNumero3.Location = new System.Drawing.Point(12, 117);
-            this.txtNumero3.Name = "txtNumero3";
-            this.txtNumero3.Size = new System.Drawing.Size(100, 26);
-            this.txtNumero3.TabIndex = 4;
-            // 
-            // Prog_Tema_3_Ej_05
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(176, 313);
-            this.Controls.Add(this.txtNumero3);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.btnAccion);
-            this.Controls.Add(this.txtNumero2);
-            this.Controls.Add(this.txtNumero1);
-            this.Name = "Prog_Tema_3_Ej_05";
-            this.Text = "Prog_Tema_3_Ej_05";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtNumero1 = new System.Windows.Forms.TextBox();
+			this.txtNumero2 = new System.Windows.Forms.TextBox();
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblResultado2 = new System.Windows.Forms.Label();
+			this.txtNumero3 = new System.Windows.Forms.TextBox();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblNum1 = new System.Windows.Forms.Label();
+			this.lblNum2 = new System.Windows.Forms.Label();
+			this.lblNum3 = new System.Windows.Forms.Label();
+			this.lblResultado1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtNumero1
+			// 
+			this.txtNumero1.Location = new System.Drawing.Point(246, 84);
+			this.txtNumero1.Name = "txtNumero1";
+			this.txtNumero1.Size = new System.Drawing.Size(100, 26);
+			this.txtNumero1.TabIndex = 1;
+			// 
+			// txtNumero2
+			// 
+			this.txtNumero2.Location = new System.Drawing.Point(246, 130);
+			this.txtNumero2.Name = "txtNumero2";
+			this.txtNumero2.Size = new System.Drawing.Size(100, 26);
+			this.txtNumero2.TabIndex = 2;
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.DeepPink;
+			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.ForeColor = System.Drawing.Color.White;
+			this.btnAccion.Location = new System.Drawing.Point(201, 228);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(113, 52);
+			this.btnAccion.TabIndex = 4;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// lblResultado2
+			// 
+			this.lblResultado2.AutoSize = true;
+			this.lblResultado2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado2.Location = new System.Drawing.Point(21, 362);
+			this.lblResultado2.Name = "lblResultado2";
+			this.lblResultado2.Size = new System.Drawing.Size(85, 26);
+			this.lblResultado2.TabIndex = 0;
+			this.lblResultado2.Text = "Detalle:";
+			// 
+			// txtNumero3
+			// 
+			this.txtNumero3.Location = new System.Drawing.Point(246, 182);
+			this.txtNumero3.Name = "txtNumero3";
+			this.txtNumero3.Size = new System.Drawing.Size(100, 26);
+			this.txtNumero3.TabIndex = 3;
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.Location = new System.Drawing.Point(102, 9);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(320, 55);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "El mayor de 3";
+			// 
+			// lblNum1
+			// 
+			this.lblNum1.AutoSize = true;
+			this.lblNum1.Location = new System.Drawing.Point(138, 87);
+			this.lblNum1.Name = "lblNum1";
+			this.lblNum1.Size = new System.Drawing.Size(82, 20);
+			this.lblNum1.TabIndex = 0;
+			this.lblNum1.Text = "Numero 1:";
+			// 
+			// lblNum2
+			// 
+			this.lblNum2.AutoSize = true;
+			this.lblNum2.Location = new System.Drawing.Point(138, 133);
+			this.lblNum2.Name = "lblNum2";
+			this.lblNum2.Size = new System.Drawing.Size(82, 20);
+			this.lblNum2.TabIndex = 0;
+			this.lblNum2.Text = "Numero 2:";
+			// 
+			// lblNum3
+			// 
+			this.lblNum3.AutoSize = true;
+			this.lblNum3.Location = new System.Drawing.Point(138, 185);
+			this.lblNum3.Name = "lblNum3";
+			this.lblNum3.Size = new System.Drawing.Size(82, 20);
+			this.lblNum3.TabIndex = 0;
+			this.lblNum3.Text = "Numero 3:";
+			// 
+			// lblResultado1
+			// 
+			this.lblResultado1.AutoSize = true;
+			this.lblResultado1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado1.Location = new System.Drawing.Point(21, 305);
+			this.lblResultado1.Name = "lblResultado1";
+			this.lblResultado1.Size = new System.Drawing.Size(108, 26);
+			this.lblResultado1.TabIndex = 0;
+			this.lblResultado1.Text = "Resultado:";
+			// 
+			// Prog_Tema_3_Ej_05
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightPink;
+			this.ClientSize = new System.Drawing.Size(541, 542);
+			this.Controls.Add(this.lblResultado1);
+			this.Controls.Add(this.lblNum3);
+			this.Controls.Add(this.lblNum2);
+			this.Controls.Add(this.lblNum1);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.txtNumero3);
+			this.Controls.Add(this.lblResultado2);
+			this.Controls.Add(this.btnAccion);
+			this.Controls.Add(this.txtNumero2);
+			this.Controls.Add(this.txtNumero1);
+			this.Name = "Prog_Tema_3_Ej_05";
+			this.Text = "Prog_Tema_3_Ej_05";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -98,7 +160,12 @@
         private System.Windows.Forms.TextBox txtNumero1;
         private System.Windows.Forms.TextBox txtNumero2;
         private System.Windows.Forms.Button btnAccion;
-        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblResultado2;
         private System.Windows.Forms.TextBox txtNumero3;
-    }
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblNum1;
+		private System.Windows.Forms.Label lblNum2;
+		private System.Windows.Forms.Label lblNum3;
+		private System.Windows.Forms.Label lblResultado1;
+		}
 }
--- Tema_03/Tema_03_Ejercicio_05/Prog_Tema_3_Ej_05/Form1.cs
+++ Tema_03/Tema_03_Ejercicio_05/Prog_Tema_3_Ej_05/Form1.cs
@@ -20,33 +20,137 @@ namespace Prog_Tema_3_Ej_05
 
         private void btnAccion_Click(object sender, EventArgs e)
         {
-            bool num1esnum = int.TryParse(txtNumero1.Text, out int numero1);
+			// int.TryParse evalúa para cada TextBox si lo introducido es un número
+			// Si lo es lo convierte en un entero
+			// Las variables num1esnum, num2esnum y num3esnum recogen un valor booleano en función de si tiene éxito la conversión a entero
+			// No se utiliza try-catch	
+			bool num1esnum = int.TryParse(txtNumero1.Text, out int numero1);
             bool num2esnum = int.TryParse(txtNumero2.Text, out int numero2);
             bool num3esnum = int.TryParse(txtNumero3.Text, out int numero3);
 
-            if (num1esnum && num2esnum && num3esnum)
+            // Declaración e inicialización de tres variables string que imprimirán el resultado tras la comparación de los números
+            string posibilidad1 = "", posibilidad2 = "", posibilidad3 = "";
+
+			// Bloque condicional if que evalúa si ha tenido éxito la conversión a entero de lo introducido en los TextBox
+			if (num1esnum && num2esnum && num3esnum)
             {
-                if ((numero1 > numero2) && (numero1 > numero2))
-                {
-                    lblResultado.Text = $"{numero1} es mayor que {numero2} y {numero3}";
-                } 
-                else if ((numero2 > numero1) && (numero2  > numero3))
-                {
-                    lblResultado.Text = $"{numero2} es mayor que {numero1} y {numero3}";
-                }
-                else if ((numero3 > numero1) && (numero3 > numero1))
-                {
-                    lblResultado.Text = $"{numero3} es mayor que {numero1} y {numero2}";
-                }
-                else
-                {
-                    lblResultado.Text = $"Los números {numero1}, {numero2}\ny {numero3} son iguales";
-                }
-            }
-            else
+
+				// Primera parte: es la que pide el enunciado del ejercicio: indicar cuál de los tres números es el mayor
+				// Se divide en tres bloques separados por comentarios para leerlo con mayor claridad
+				// pero todo está dentro de un gran bloque if con diversos else if encadenados de forma que se evalúan todas las posibilidades
+				// menos una que se deja a la claúsula else
+
+				// 1ª Evaluación: todos los números son diferentes
+				if ((numero1 > numero2) && (numero1 > numero3))
+					{
+					lblResultado1.Text = $"Número 1 ({numero1}) es mayor que Número 2 ({numero2}) y Número 3 ({numero3})";
+					}
+				else if ((numero2 > numero1) && (numero2 > numero3))
+					{
+					lblResultado1.Text = $"Número 2 ({numero2}) es mayor que Número 1 ({numero1}) y Número 3 ({numero3})";
+					}
+				else if ((numero3 > numero1) && (numero3 > numero2))
+					{
+					lblResultado1.Text = $"Número 3 ({numero3}) es mayor que Número 1 ({numero1}) y Número 2 ({numero2})";
+					}
+
+				// 2ª Evaluación: dos números iguales y mayores que el tercero
+				else if ((numero1 == numero2) && (numero1 > numero3))
+					{
+					lblResultado1.Text = $"Los números Número 1 ({numero1}) y Número 2 ({numero2}) son iguales\ny mayores que el Número 3 ({numero3}) ";
+					}
+				else if ((numero1 == numero3) && (numero1 > numero2))
+					{
+					lblResultado1.Text = $"Los números Número 1 ({numero1}) y Número 3 ({numero3}) son iguales\ny mayores que el Número 2 ({numero2}) ";
+					}
+				else if ((numero2 == numero3) && (numero2 > numero1))
+					{
+					lblResultado1.Text = $"Los números Número 2 ({numero2}) y Número 3 ({numero3}) son iguales\ny mayores que el Número 1 ({numero1}) ";
+					}
+
+				// 3ª Evaluación: os números iguales y menores que el tercero
+				else if ((numero1 == numero2) && (numero1 < numero3))
+					{
+					lblResultado1.Text = $"Los números Número 1 ({numero1}) y Número 2 ({numero2}) son iguales\ny menores que el Número 3 ({numero3}) ";
+					}
+				else if ((numero1 == numero3) && (numero1 < numero2))
+					{
+					lblResultado1.Text = $"Los números Número 1 ({numero1}) y Número 3 ({numero3}) son iguales\ny menores que el Número 2 ({numero2}) ";
+					}
+				else if ((numero2 == numero3) && (numero3 < numero1))
+					{
+					lblResultado1.Text = $"Los números Número 2 ({numero2}) y Número 3 ({numero3}) son iguales\ny menores que el Número 1 ({numero1}) ";
+					}
+
+
+				// La última alternativa: que sean iguales los tres números
+				else
+					{
+					lblResultado1.Text = $"Los números Número 1 ({numero1}), Número 2 ({numero2})\ny Número 3 ({numero3}) son iguales";
+					}
+
+
+
+				// Segunda parte: añadido al ejercicio. El programa detalla la comparación entre números.
+
+				// Se evalúa la relación entre los números en tres bloques if
+				// En cada bloque se comparan los números por parejas; en función del resultado el string toma un valor u otro ("igual", "menor", "mayor")
+
+				// Relación entre numero1 y numero2
+				if (numero1 == numero2)
+					{
+						posibilidad1 = "igual";
+					} 
+					else if (numero1 < numero2)
+					{
+						posibilidad1 = "menor";
+					}
+					else if (numero1 > numero2)
+					{
+						posibilidad1 = "mayor";
+					}
+
+					// Relación entre numero1 y numero3
+					if (numero1 == numero3)
+					{
+						posibilidad2 = "igual";
+					}
+					else if (numero1 < numero3)
+					{
+						posibilidad2 = "menor";
+					}
+					else if (numero1 > numero3)
+					{
+						posibilidad2 = "mayor";
+					}
+
+					// Relación entre numero2 y numero3
+					if (numero2 == numero3)
+					{
+						posibilidad3 = "igual";
+					}
+					else if (numero2 < numero3)
+					{
+						posibilidad3 = "menor";
+					}
+					else if (numero2 > numero3)
+					{
+						posibilidad3 = "mayor";
+					}
+
+				lblResultado2.Text = $"El Número 1 ({numero1}) es {posibilidad1} que el Número 2 ({numero2})\n" +
+				$"El Número 1 ({numero1}) es {posibilidad2} que el Número 3 ({numero3})\n" +
+				$"El Número 2 ({numero2}) es {posibilidad3} que el Número 3 ({numero3})";
+				}
+
+			else
             {
-                lblResultado.Text = "Introduce números";
+                lblResultado2.Text = "Introduce números";
             }
+
+
+
+
 
         }
 
