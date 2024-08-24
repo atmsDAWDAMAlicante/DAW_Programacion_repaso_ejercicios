8ad4b84c63f19bb83aee0dd1d5a5804b48bc6a19
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-24 11:35:37 +0200

    Commit inicial

---
+++ Tema_06/Tema_06_Ejercicio_05/Prog_Tema_6_Ej_05/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_6_Ej_05
+	{
+	public partial class Prog_Tema_6_Ej_05 : Form
+		{
+		public Prog_Tema_6_Ej_05()
+			{
+			InitializeComponent();
+			}
+		}
+	}
---
+++ Tema_06/Tema_06_Ejercicio_08/Prog_Tema_6_Ej_08/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_Tema_6_Ej_08
+	{
+	public partial class Prog_Tema_6_Ej_08 : Form
+		{
+		public Prog_Tema_6_Ej_08()
+			{
+			InitializeComponent();
+			}
+		}
+	}
