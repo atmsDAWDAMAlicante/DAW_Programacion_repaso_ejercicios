ab45ddfc03e152f3898aaed1e2b27cd8fed1fb61
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-25 09:04:40 +0200

    Diseño del Form

--- Tema_03/Tema_03_Ejercicio_09/Prog_Tema_3_Ej_09/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_09/Prog_Tema_3_Ej_09/Form1.Designer.cs
@@ -28,73 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTitulo = new System.Windows.Forms.Label();
-            this.txtCantidad = new System.Windows.Forms.TextBox();
-            this.btnSuCambio = new System.Windows.Forms.Button();
-            this.grpSuCambio = new System.Windows.Forms.GroupBox();
-            this.lblResultado = new System.Windows.Forms.Label();
-            this.grpSuCambio.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // lblTitulo
-            // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(12, 20);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(256, 20);
-            this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Introduce una cantidad en pesetas";
-            // 
-            // txtCantidad
-            // 
-            this.txtCantidad.Location = new System.Drawing.Point(15, 59);
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(138, 26);
-            this.txtCantidad.TabIndex = 1;
-            // 
-            // btnSuCambio
-            // 
-            this.btnSuCambio.Location = new System.Drawing.Point(159, 55);
-            this.btnSuCambio.Name = "btnSuCambio";
-            this.btnSuCambio.Size = new System.Drawing.Size(109, 37);
-            this.btnSuCambio.TabIndex = 2;
-            this.btnSuCambio.Text = "Su cambio";
-            this.btnSuCambio.UseVisualStyleBackColor = true;
-            this.btnSuCambio.Click += new System.EventHandler(this.btnSuCambio_Click);
-            // 
-            // grpSuCambio
-            // 
-            this.grpSuCambio.Controls.Add(this.lblResultado);
-            this.grpSuCambio.Location = new System.Drawing.Point(16, 98);
-            this.grpSuCambio.Name = "grpSuCambio";
-            this.grpSuCambio.Size = new System.Drawing.Size(252, 332);
-            this.grpSuCambio.TabIndex = 0;
-            this.grpSuCambio.TabStop = false;
-            this.grpSuCambio.Text = "Su cambio:";
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(6, 35);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(0, 24);
-            this.lblResultado.TabIndex = 0;
-            // 
-            // Prog_Tema_3_Ej_09
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 450);
-            this.Controls.Add(this.grpSuCambio);
-            this.Controls.Add(this.btnSuCambio);
-            this.Controls.Add(this.txtCantidad);
-            this.Controls.Add(this.lblTitulo);
-            this.Name = "Prog_Tema_3_Ej_09";
-            this.Text = "Prog_Tema_3_Ej_09";
-            this.grpSuCambio.ResumeLayout(false);
-            this.grpSuCambio.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.txtCantidad = new System.Windows.Forms.TextBox();
+			this.btnSuCambio = new System.Windows.Forms.Button();
+			this.grpSuCambio = new System.Windows.Forms.GroupBox();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.grpSuCambio.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.Bisque;
+			this.lblTitulo.Location = new System.Drawing.Point(12, 13);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(379, 29);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "Introduce una cantidad en pesetas";
+			// 
+			// txtCantidad
+			// 
+			this.txtCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCantidad.ForeColor = System.Drawing.Color.Firebrick;
+			this.txtCantidad.Location = new System.Drawing.Point(12, 59);
+			this.txtCantidad.Name = "txtCantidad";
+			this.txtCantidad.Size = new System.Drawing.Size(185, 30);
+			this.txtCantidad.TabIndex = 1;
+			// 
+			// btnSuCambio
+			// 
+			this.btnSuCambio.BackColor = System.Drawing.Color.OrangeRed;
+			this.btnSuCambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSuCambio.ForeColor = System.Drawing.Color.Bisque;
+			this.btnSuCambio.Location = new System.Drawing.Point(222, 55);
+			this.btnSuCambio.Name = "btnSuCambio";
+			this.btnSuCambio.Size = new System.Drawing.Size(171, 42);
+			this.btnSuCambio.TabIndex = 2;
+			this.btnSuCambio.Text = "Calcular";
+			this.btnSuCambio.UseVisualStyleBackColor = false;
+			this.btnSuCambio.Click += new System.EventHandler(this.btnSuCambio_Click);
+			// 
+			// grpSuCambio
+			// 
+			this.grpSuCambio.Controls.Add(this.lblResultado);
+			this.grpSuCambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpSuCambio.ForeColor = System.Drawing.Color.Bisque;
+			this.grpSuCambio.Location = new System.Drawing.Point(12, 98);
+			this.grpSuCambio.Name = "grpSuCambio";
+			this.grpSuCambio.Size = new System.Drawing.Size(379, 332);
+			this.grpSuCambio.TabIndex = 0;
+			this.grpSuCambio.TabStop = false;
+			this.grpSuCambio.Text = "Su cambio:";
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(6, 35);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(88, 25);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "pesetas";
+			// 
+			// Prog_Tema_3_Ej_09
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Firebrick;
+			this.ClientSize = new System.Drawing.Size(422, 450);
+			this.Controls.Add(this.grpSuCambio);
+			this.Controls.Add(this.btnSuCambio);
+			this.Controls.Add(this.txtCantidad);
+			this.Controls.Add(this.lblTitulo);
+			this.Name = "Prog_Tema_3_Ej_09";
+			this.Text = "Prog_Tema_3_Ej_09";
+			this.grpSuCambio.ResumeLayout(false);
+			this.grpSuCambio.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
