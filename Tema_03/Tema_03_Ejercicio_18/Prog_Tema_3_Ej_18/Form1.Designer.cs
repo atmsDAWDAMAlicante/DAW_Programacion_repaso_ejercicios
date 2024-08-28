e8fa35f5bb2e4a245a2445c6b1060d7564e44aa8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-28 19:33:19 +0200

    Diseño del Form

--- Tema_03/Tema_03_Ejercicio_18/Prog_Tema_3_Ej_18/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_18/Prog_Tema_3_Ej_18/Form1.Designer.cs
@@ -28,20 +28,79 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.btnAccion = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Comic Sans MS", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.Location = new System.Drawing.Point(79, 10);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(241, 78);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "Σ Suma";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.Location = new System.Drawing.Point(69, 91);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(261, 70);
+			this.lblSubtitulo.TabIndex = 0;
+			this.lblSubtitulo.Text = "de los números pares\ndel 1 al 50";
+			this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Comic Sans MS", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(62, 266);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(290, 78);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "Resultado";
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.SaddleBrown;
+			this.btnAccion.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.ForeColor = System.Drawing.Color.Bisque;
+			this.btnAccion.Location = new System.Drawing.Point(120, 171);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(163, 83);
+			this.btnAccion.TabIndex = 1;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
 			// 
 			// Prog_Tema_3_Ej_18
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.Bisque;
+			this.ClientSize = new System.Drawing.Size(425, 396);
+			this.Controls.Add(this.btnAccion);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.lblTitulo);
+			this.ForeColor = System.Drawing.Color.Maroon;
 			this.Name = "Prog_Tema_3_Ej_18";
 			this.Text = "Prog_Tema_3_Ej_18";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblSubtitulo;
+		private System.Windows.Forms.Label lblResultado;
+		private System.Windows.Forms.Button btnAccion;
 		}
 	}
 
