bcc71f0d1f9544983822812bd1840cbdc00818a5
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-28 16:39:44 +0200

    Diseño del Form

--- Tema_03/Tema_03_Ejercicio_17/Prog_Tema_3_Ej_17/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_17/Prog_Tema_3_Ej_17/Form1.Designer.cs
@@ -28,20 +28,79 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.White;
+			this.lblTitulo.Location = new System.Drawing.Point(47, 22);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(319, 51);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "Múltiplos de 3";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.ForeColor = System.Drawing.Color.White;
+			this.lblSubtitulo.Location = new System.Drawing.Point(102, 84);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(191, 32);
+			this.lblSubtitulo.TabIndex = 0;
+			this.lblSubtitulo.Text = "entre 1 y 100";
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.NavajoWhite;
+			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.Location = new System.Drawing.Point(117, 138);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(152, 57);
+			this.btnAccion.TabIndex = 1;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.ForeColor = System.Drawing.Color.White;
+			this.lblResultado.Location = new System.Drawing.Point(128, 222);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(128, 28);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "Resultado";
 			// 
 			// Prog_Tema_3_Ej_17
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.Tomato;
+			this.ClientSize = new System.Drawing.Size(410, 352);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnAccion);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.lblTitulo);
 			this.Name = "Prog_Tema_3_Ej_17";
 			this.Text = "Prog_Tema_3_Ej_17";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblSubtitulo;
+		private System.Windows.Forms.Button btnAccion;
+		private System.Windows.Forms.Label lblResultado;
 		}
 	}
 
