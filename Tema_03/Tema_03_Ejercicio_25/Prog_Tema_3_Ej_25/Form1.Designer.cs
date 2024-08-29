656472c43c54075090f96ef3512c5803265f722e
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-29 20:33:11 +0200

    Diseño del Form

--- Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.Designer.cs
@@ -28,20 +28,126 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.btnMostrar = new System.Windows.Forms.Button();
+			this.lblResultado1 = new System.Windows.Forms.Label();
+			this.grpPizarraIzq = new System.Windows.Forms.GroupBox();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.grpPizarraDcha = new System.Windows.Forms.GroupBox();
+			this.lblResultado2 = new System.Windows.Forms.Label();
+			this.grpPizarraIzq.SuspendLayout();
+			this.grpPizarraDcha.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.White;
+			this.lblTitulo.Location = new System.Drawing.Point(12, -1);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(466, 50);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "Las Tablas de Multiplicar";
+			// 
+			// btnMostrar
+			// 
+			this.btnMostrar.BackColor = System.Drawing.Color.DimGray;
+			this.btnMostrar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnMostrar.ForeColor = System.Drawing.Color.White;
+			this.btnMostrar.Location = new System.Drawing.Point(171, 77);
+			this.btnMostrar.Name = "btnMostrar";
+			this.btnMostrar.Size = new System.Drawing.Size(154, 46);
+			this.btnMostrar.TabIndex = 2;
+			this.btnMostrar.Text = "Mostrar";
+			this.btnMostrar.UseVisualStyleBackColor = false;
+			// 
+			// lblResultado1
+			// 
+			this.lblResultado1.AutoSize = true;
+			this.lblResultado1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado1.ForeColor = System.Drawing.Color.White;
+			this.lblResultado1.Location = new System.Drawing.Point(17, 29);
+			this.lblResultado1.Name = "lblResultado1";
+			this.lblResultado1.Size = new System.Drawing.Size(91, 20);
+			this.lblResultado1.TabIndex = 0;
+			this.lblResultado1.Text = "Resultado";
+			// 
+			// grpPizarraIzq
+			// 
+			this.grpPizarraIzq.Controls.Add(this.lblResultado1);
+			this.grpPizarraIzq.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarraIzq.ForeColor = System.Drawing.Color.White;
+			this.grpPizarraIzq.Location = new System.Drawing.Point(18, 129);
+			this.grpPizarraIzq.Name = "grpPizarraIzq";
+			this.grpPizarraIzq.Size = new System.Drawing.Size(218, 463);
+			this.grpPizarraIzq.TabIndex = 0;
+			this.grpPizarraIzq.TabStop = false;
+			this.grpPizarraIzq.Text = "Pizarra";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.ForeColor = System.Drawing.Color.White;
+			this.lblSubtitulo.Location = new System.Drawing.Point(189, 49);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(110, 25);
+			this.lblSubtitulo.TabIndex = 3;
+			this.lblSubtitulo.Text = "del 1 al 10";
+			// 
+			// grpPizarraDcha
+			// 
+			this.grpPizarraDcha.Controls.Add(this.lblResultado2);
+			this.grpPizarraDcha.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarraDcha.ForeColor = System.Drawing.Color.White;
+			this.grpPizarraDcha.Location = new System.Drawing.Point(254, 129);
+			this.grpPizarraDcha.Name = "grpPizarraDcha";
+			this.grpPizarraDcha.Size = new System.Drawing.Size(218, 463);
+			this.grpPizarraDcha.TabIndex = 0;
+			this.grpPizarraDcha.TabStop = false;
+			// 
+			// lblResultado2
+			// 
+			this.lblResultado2.AutoSize = true;
+			this.lblResultado2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado2.ForeColor = System.Drawing.Color.White;
+			this.lblResultado2.Location = new System.Drawing.Point(17, 29);
+			this.lblResultado2.Name = "lblResultado2";
+			this.lblResultado2.Size = new System.Drawing.Size(72, 20);
+			this.lblResultado2.TabIndex = 0;
+			this.lblResultado2.Text = "... sigue";
 			// 
 			// Prog_Tema_3_Ej_25
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(484, 604);
+			this.Controls.Add(this.grpPizarraDcha);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.grpPizarraIzq);
+			this.Controls.Add(this.btnMostrar);
+			this.Controls.Add(this.lblTitulo);
 			this.Name = "Prog_Tema_3_Ej_25";
 			this.Text = "Prog_Tema_3_Ej_25";
+			this.grpPizarraIzq.ResumeLayout(false);
+			this.grpPizarraIzq.PerformLayout();
+			this.grpPizarraDcha.ResumeLayout(false);
+			this.grpPizarraDcha.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Button btnMostrar;
+		private System.Windows.Forms.Label lblResultado1;
+		private System.Windows.Forms.GroupBox grpPizarraIzq;
+		private System.Windows.Forms.Label lblSubtitulo;
+		private System.Windows.Forms.GroupBox grpPizarraDcha;
+		private System.Windows.Forms.Label lblResultado2;
 		}
 	}
-
