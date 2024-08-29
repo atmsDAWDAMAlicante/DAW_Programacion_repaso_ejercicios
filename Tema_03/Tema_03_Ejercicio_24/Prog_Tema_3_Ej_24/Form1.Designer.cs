e8998798347d1fee3cd8eabfef797f70a2e425ef
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-29 20:20:53 +0200

    Diseño del Form

--- Tema_03/Tema_03_Ejercicio_24/Prog_Tema_3_Ej_24/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_24/Prog_Tema_3_Ej_24/Form1.Designer.cs
@@ -28,20 +28,87 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.txtNumero = new System.Windows.Forms.TextBox();
+			this.btnMostrar = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.Location = new System.Drawing.Point(25, 18);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(402, 40);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "La Tabla de Multiplicar";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.Location = new System.Drawing.Point(53, 79);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(159, 29);
+			this.lblSubtitulo.TabIndex = 0;
+			this.lblSubtitulo.Text = "del número: ";
+			// 
+			// txtNumero
+			// 
+			this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNumero.Location = new System.Drawing.Point(218, 79);
+			this.txtNumero.Name = "txtNumero";
+			this.txtNumero.Size = new System.Drawing.Size(70, 30);
+			this.txtNumero.TabIndex = 1;
+			// 
+			// btnMostrar
+			// 
+			this.btnMostrar.BackColor = System.Drawing.Color.Yellow;
+			this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnMostrar.Location = new System.Drawing.Point(117, 137);
+			this.btnMostrar.Name = "btnMostrar";
+			this.btnMostrar.Size = new System.Drawing.Size(192, 58);
+			this.btnMostrar.TabIndex = 2;
+			this.btnMostrar.Text = "Mostrar";
+			this.btnMostrar.UseVisualStyleBackColor = false;
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(70, 234);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(82, 20);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "Resultado";
 			// 
 			// Prog_Tema_3_Ej_24
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.PaleGoldenrod;
+			this.ClientSize = new System.Drawing.Size(484, 604);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnMostrar);
+			this.Controls.Add(this.txtNumero);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.lblTitulo);
 			this.Name = "Prog_Tema_3_Ej_24";
 			this.Text = "Prog_Tema_3_Ej_24";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblSubtitulo;
+		private System.Windows.Forms.TextBox txtNumero;
+		private System.Windows.Forms.Button btnMostrar;
+		private System.Windows.Forms.Label lblResultado;
 		}
 	}
 
