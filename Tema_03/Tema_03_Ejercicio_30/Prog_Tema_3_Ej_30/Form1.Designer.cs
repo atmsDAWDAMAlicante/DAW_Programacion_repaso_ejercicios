fa276e621c1deb5197e0ae9b68cfe99929fe370e
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-30 16:49:24 +0200

    Diseño del Form

--- Tema_03/Tema_03_Ejercicio_30/Prog_Tema_3_Ej_30/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_30/Prog_Tema_3_Ej_30/Form1.Designer.cs
@@ -28,20 +28,127 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.txtUsuario = new System.Windows.Forms.TextBox();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.btnAcceder = new System.Windows.Forms.Button();
+			this.lblUsuario = new System.Windows.Forms.Label();
+			this.lblPassword = new System.Windows.Forms.Label();
+			this.lblResultado = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+			this.lblTitulo.Location = new System.Drawing.Point(25, 36);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(111, 49);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "Login";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.ForeColor = System.Drawing.Color.Black;
+			this.lblSubtitulo.Location = new System.Drawing.Point(142, 51);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(217, 29);
+			this.lblSubtitulo.TabIndex = 0;
+			this.lblSubtitulo.Text = "Tienes tres intentos: ";
+			// 
+			// txtUsuario
+			// 
+			this.txtUsuario.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUsuario.Location = new System.Drawing.Point(155, 106);
+			this.txtUsuario.Name = "txtUsuario";
+			this.txtUsuario.Size = new System.Drawing.Size(125, 32);
+			this.txtUsuario.TabIndex = 1;
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtPassword.Location = new System.Drawing.Point(155, 153);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.Size = new System.Drawing.Size(125, 32);
+			this.txtPassword.TabIndex = 2;
+			// 
+			// btnAcceder
+			// 
+			this.btnAcceder.BackColor = System.Drawing.Color.YellowGreen;
+			this.btnAcceder.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAcceder.Location = new System.Drawing.Point(317, 114);
+			this.btnAcceder.Name = "btnAcceder";
+			this.btnAcceder.Size = new System.Drawing.Size(141, 58);
+			this.btnAcceder.TabIndex = 3;
+			this.btnAcceder.Text = "Acceder";
+			this.btnAcceder.UseVisualStyleBackColor = false;
+			// 
+			// lblUsuario
+			// 
+			this.lblUsuario.AutoSize = true;
+			this.lblUsuario.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblUsuario.ForeColor = System.Drawing.Color.Black;
+			this.lblUsuario.Location = new System.Drawing.Point(30, 114);
+			this.lblUsuario.Name = "lblUsuario";
+			this.lblUsuario.Size = new System.Drawing.Size(81, 24);
+			this.lblUsuario.TabIndex = 0;
+			this.lblUsuario.Text = "Usuario:";
+			// 
+			// lblPassword
+			// 
+			this.lblPassword.AutoSize = true;
+			this.lblPassword.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPassword.ForeColor = System.Drawing.Color.Black;
+			this.lblPassword.Location = new System.Drawing.Point(30, 156);
+			this.lblPassword.Name = "lblPassword";
+			this.lblPassword.Size = new System.Drawing.Size(112, 24);
+			this.lblPassword.TabIndex = 0;
+			this.lblPassword.Text = "Contraseña:";
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(30, 209);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(185, 23);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "Resultado: root / 1234";
 			// 
 			// Prog_Tema_3_Ej_30
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.MediumSpringGreen;
+			this.ClientSize = new System.Drawing.Size(484, 238);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.lblPassword);
+			this.Controls.Add(this.lblUsuario);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.btnAcceder);
+			this.Controls.Add(this.txtUsuario);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.lblTitulo);
 			this.Name = "Prog_Tema_3_Ej_30";
 			this.Text = "Prog_Tema_3_Ej_30";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblSubtitulo;
+		private System.Windows.Forms.TextBox txtUsuario;
+		private System.Windows.Forms.Button btnAcceder;
+		private System.Windows.Forms.TextBox txtPassword;
+		private System.Windows.Forms.Label lblUsuario;
+		private System.Windows.Forms.Label lblPassword;
+		private System.Windows.Forms.Label lblResultado;
 		}
 	}
-
