5da5ececad0785e6709d5a373bb9fcc696ccbf36
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-31 20:20:18 +0200

    Programa completo

--- Tema_03/Tema_03_Ejercicio_23/Prog_Tema_3_Ej_23/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_23/Prog_Tema_3_Ej_23/Form1.Designer.cs
@@ -32,16 +32,17 @@
 			this.lblSubtitulo = new System.Windows.Forms.Label();
 			this.lblResultado = new System.Windows.Forms.Label();
 			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblAlerta = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// lblTitulo
 			// 
 			this.lblTitulo.AutoSize = true;
-			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lblTitulo.ForeColor = System.Drawing.Color.White;
-			this.lblTitulo.Location = new System.Drawing.Point(14, 27);
+			this.lblTitulo.Location = new System.Drawing.Point(14, 20);
 			this.lblTitulo.Name = "lblTitulo";
-			this.lblTitulo.Size = new System.Drawing.Size(531, 59);
+			this.lblTitulo.Size = new System.Drawing.Size(570, 64);
 			this.lblTitulo.TabIndex = 0;
 			this.lblTitulo.Text = "Peso de los Alumnos";
 			// 
@@ -50,7 +51,7 @@
 			this.lblSubtitulo.AutoSize = true;
 			this.lblSubtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lblSubtitulo.ForeColor = System.Drawing.Color.White;
-			this.lblSubtitulo.Location = new System.Drawing.Point(14, 120);
+			this.lblSubtitulo.Location = new System.Drawing.Point(20, 80);
 			this.lblSubtitulo.Name = "lblSubtitulo";
 			this.lblSubtitulo.Size = new System.Drawing.Size(329, 50);
 			this.lblSubtitulo.TabIndex = 0;
@@ -61,7 +62,7 @@
 			this.lblResultado.AutoSize = true;
 			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lblResultado.ForeColor = System.Drawing.Color.White;
-			this.lblResultado.Location = new System.Drawing.Point(14, 221);
+			this.lblResultado.Location = new System.Drawing.Point(20, 191);
 			this.lblResultado.Name = "lblResultado";
 			this.lblResultado.Size = new System.Drawing.Size(131, 29);
 			this.lblResultado.TabIndex = 0;
@@ -70,19 +71,32 @@
 			// btnAccion
 			// 
 			this.btnAccion.BackColor = System.Drawing.Color.DeepSkyBlue;
-			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.btnAccion.ForeColor = System.Drawing.Color.White;
-			this.btnAccion.Location = new System.Drawing.Point(375, 120);
+			this.btnAccion.Location = new System.Drawing.Point(280, 80);
 			this.btnAccion.Name = "btnAccion";
-			this.btnAccion.Size = new System.Drawing.Size(295, 76);
+			this.btnAccion.Size = new System.Drawing.Size(180, 50);
 			this.btnAccion.TabIndex = 1;
 			this.btnAccion.Text = "Insertar pesos";
 			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// lblAlerta
+			// 
+			this.lblAlerta.AutoSize = true;
+			this.lblAlerta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblAlerta.ForeColor = System.Drawing.Color.Red;
+			this.lblAlerta.Location = new System.Drawing.Point(20, 147);
+			this.lblAlerta.Name = "lblAlerta";
+			this.lblAlerta.Size = new System.Drawing.Size(51, 20);
+			this.lblAlerta.TabIndex = 0;
+			this.lblAlerta.Text = "Alerta";
 			// 
 			// Prog_Tema_3_Ej_23
 			// 
 			this.BackColor = System.Drawing.Color.LightSkyBlue;
-			this.ClientSize = new System.Drawing.Size(712, 413);
+			this.ClientSize = new System.Drawing.Size(500, 413);
+			this.Controls.Add(this.lblAlerta);
 			this.Controls.Add(this.btnAccion);
 			this.Controls.Add(this.lblTitulo);
 			this.Controls.Add(this.lblSubtitulo);
@@ -100,6 +114,7 @@
 		private System.Windows.Forms.Label lblSubtitulo;
 		private System.Windows.Forms.Label lblResultado;
 		private System.Windows.Forms.Button btnAccion;
+		private System.Windows.Forms.Label lblAlerta;
 		}
 	}
 
--- Tema_03/Tema_03_Ejercicio_23/Prog_Tema_3_Ej_23/Form1.cs
+++ Tema_03/Tema_03_Ejercicio_23/Prog_Tema_3_Ej_23/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace Prog_Tema_3_Ej_23
 	{
@@ -15,6 +16,87 @@ namespace Prog_Tema_3_Ej_23
 		public Prog_Tema_3_Ej_23()
 			{
 			InitializeComponent();
+			}
+
+		private void btnAccion_Click(object sender, EventArgs e)
+			{
+			// Se declaran las siguientes variables entero y double y se inicializan todas a 0
+			// Variable peso que es la que recogerá los pesos que se introduzcan mediante el InputBox
+			double peso;
+
+			// Variables que recogen los números de los alumnos por grupo. De tipo entero
+			int numAlumnosMenos50 = 0;
+			int numAlumnos50a65 = 0;
+			int numAlumnos65a80 = 0;
+			int numAlumnosMas80 = 0;
+			int numAlumnosTotal = 0; // la variable total que recogerá la suma de las anteriores
+
+			// Variables que recogen los pesos de los alumnos por grupo. De tipo double
+			double pesoAlumnosMenos50 = 0;
+			double pesoAlumnos50a65 = 0;
+			double pesoAlumnos65a80 = 0;
+			double pesoAlumnosMas80 = 0;
+			double pesoAlumnosTotal = 0; // la variable total que recogerá la suma de las anteriores
+
+			lblAlerta.Text = "";
+			lblResultado.Text = "";
+
+
+			// Bucle Do...While que se repetirá hasta que se introduzca un número negativo en el InputBox
+			// Al tratarse de un bucle Do...While se ejecutará por lo menos una vez
+			do {
+				bool numEsNum = double.TryParse(Interaction.InputBox("Introduce un peso"), out peso);
+				if (numEsNum) // Cada vez que se introduzca un valor no válido se reiniciará el recuento de pesos/alumnos
+					{ // (<=50), (>50 y <=65), (>65 y <=80), (>80)
+					if ((peso > 0) && (peso <= 50))
+						{
+						pesoAlumnosMenos50 += peso; // si el peso introducido pertenece al rango, se acumula al total de pesos de esta categoría
+						numAlumnosMenos50++; // se incrementa el número de alumnos de este rango
+						lblAlerta.Text = "";
+						}
+					else if ((peso > 50) && (peso <= 65))
+						{
+						pesoAlumnos50a65 += peso; // si el peso introducido pertenece al rango, se acumula al total de pesos de esta categoría
+						numAlumnos50a65++; // se incrementa el número de alumnos de este rango
+						lblAlerta.Text = "";
+						}
+					else if ((peso > 65) && (peso <= 80))
+						{
+						pesoAlumnos65a80 += peso; // si el peso introducido pertenece al rango, se acumula al total de pesos de esta categoría
+						numAlumnos65a80++; // se incrementa el número de alumnos de este rango
+						lblAlerta.Text = "";
+						}
+					else if (peso > 80)
+						{
+						pesoAlumnosMas80 += peso; // si el peso introducido pertenece al rango, se acumula al total de pesos de esta categoría
+						numAlumnosMas80++; // se incrementa el número de alumnos de este rango
+						lblAlerta.Text = "";
+						}
+					}
+				else
+					{
+					lblAlerta.Text = "Introduce números";
+					}
+
+
+				} while (peso > 0);
+
+			// Introducido un número negativo en el InputBox se sale del bucle Do...While
+			// Se suman todos los números de los distintos grupos de alumnos
+			numAlumnosTotal = numAlumnosMenos50 + numAlumnos50a65 + numAlumnos65a80 + numAlumnosMas80;
+			// Se suman todos los pesos de los distintos grupos de alumnos
+			pesoAlumnosTotal = pesoAlumnosMenos50 + pesoAlumnos50a65 + pesoAlumnos65a80 + pesoAlumnosMas80;
+
+			// Se actualiza el label que muestra el resultado mediante plantillas literales
+			// Para cada grupo de alumnos se muestra su total y el promedio que representa sobre el total de alumnos
+			// se utiliza una fórmula del % ((numero total de cada grupo * 100)/ numero total de alumnos de toda el aula).
+			// A efectos de un redondeo del resultado, se han utilizado enteros en las variables relativas a los números de alumnos
+			lblResultado.Text = $"Totales:\n{numAlumnosMenos50} alumnos con un peso inferior a 50 kg ({(numAlumnosMenos50*100)/numAlumnosTotal}%)\n" +
+				$"{numAlumnos50a65} alumnos con un peso entre 51 y 65 kg ({(numAlumnos50a65*100) / numAlumnosTotal}%)\n" +
+				$"{numAlumnos65a80} alumnos con un peso entre 66 y 80 kg ({(numAlumnos65a80*100) / numAlumnosTotal}%)\n" +
+				$"{numAlumnosMas80} alumnos con un peso superior a 80 kg ({(numAlumnosMas80*100) / numAlumnosTotal}%)\n" +
+				$"Total de alumnos: {numAlumnosTotal} alumnos\n" +
+				$"Media del peso del aula: {pesoAlumnosTotal/numAlumnosTotal} kg";
 			}
 		}
 	}
