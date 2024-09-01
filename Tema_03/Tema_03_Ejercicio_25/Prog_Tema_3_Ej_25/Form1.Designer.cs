d0b0b70f696e503565a680c521f2940dba8f1c20
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-01 10:15:42 +0200

    Programa completo

--- Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.Designer.cs
+++ Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.Designer.cs
@@ -31,12 +31,21 @@
 			this.lblTitulo = new System.Windows.Forms.Label();
 			this.btnMostrar = new System.Windows.Forms.Button();
 			this.lblResultado1 = new System.Windows.Forms.Label();
-			this.grpPizarraIzq = new System.Windows.Forms.GroupBox();
+			this.grpPizarra1 = new System.Windows.Forms.GroupBox();
 			this.lblSubtitulo = new System.Windows.Forms.Label();
-			this.grpPizarraDcha = new System.Windows.Forms.GroupBox();
+			this.grpPizarra2 = new System.Windows.Forms.GroupBox();
 			this.lblResultado2 = new System.Windows.Forms.Label();
-			this.grpPizarraIzq.SuspendLayout();
-			this.grpPizarraDcha.SuspendLayout();
+			this.grpPizarra4 = new System.Windows.Forms.GroupBox();
+			this.lblResultado4 = new System.Windows.Forms.Label();
+			this.grpPizarra3 = new System.Windows.Forms.GroupBox();
+			this.lblResultado3 = new System.Windows.Forms.Label();
+			this.grpPizarra5 = new System.Windows.Forms.GroupBox();
+			this.lblResultado5 = new System.Windows.Forms.Label();
+			this.grpPizarra1.SuspendLayout();
+			this.grpPizarra2.SuspendLayout();
+			this.grpPizarra4.SuspendLayout();
+			this.grpPizarra3.SuspendLayout();
+			this.grpPizarra5.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lblTitulo
@@ -61,6 +70,7 @@
 			this.btnMostrar.TabIndex = 2;
 			this.btnMostrar.Text = "Mostrar";
 			this.btnMostrar.UseVisualStyleBackColor = false;
+			this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
 			// 
 			// lblResultado1
 			// 
@@ -73,17 +83,17 @@
 			this.lblResultado1.TabIndex = 0;
 			this.lblResultado1.Text = "Resultado";
 			// 
-			// grpPizarraIzq
+			// grpPizarra1
 			// 
-			this.grpPizarraIzq.Controls.Add(this.lblResultado1);
-			this.grpPizarraIzq.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.grpPizarraIzq.ForeColor = System.Drawing.Color.White;
-			this.grpPizarraIzq.Location = new System.Drawing.Point(18, 129);
-			this.grpPizarraIzq.Name = "grpPizarraIzq";
-			this.grpPizarraIzq.Size = new System.Drawing.Size(218, 463);
-			this.grpPizarraIzq.TabIndex = 0;
-			this.grpPizarraIzq.TabStop = false;
-			this.grpPizarraIzq.Text = "Pizarra";
+			this.grpPizarra1.Controls.Add(this.lblResultado1);
+			this.grpPizarra1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarra1.ForeColor = System.Drawing.Color.White;
+			this.grpPizarra1.Location = new System.Drawing.Point(18, 129);
+			this.grpPizarra1.Name = "grpPizarra1";
+			this.grpPizarra1.Size = new System.Drawing.Size(218, 475);
+			this.grpPizarra1.TabIndex = 0;
+			this.grpPizarra1.TabStop = false;
+			this.grpPizarra1.Text = "Pizarra";
 			// 
 			// lblSubtitulo
 			// 
@@ -96,16 +106,16 @@
 			this.lblSubtitulo.TabIndex = 3;
 			this.lblSubtitulo.Text = "del 1 al 10";
 			// 
-			// grpPizarraDcha
+			// grpPizarra2
 			// 
-			this.grpPizarraDcha.Controls.Add(this.lblResultado2);
-			this.grpPizarraDcha.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.grpPizarraDcha.ForeColor = System.Drawing.Color.White;
-			this.grpPizarraDcha.Location = new System.Drawing.Point(254, 129);
-			this.grpPizarraDcha.Name = "grpPizarraDcha";
-			this.grpPizarraDcha.Size = new System.Drawing.Size(218, 463);
-			this.grpPizarraDcha.TabIndex = 0;
-			this.grpPizarraDcha.TabStop = false;
+			this.grpPizarra2.Controls.Add(this.lblResultado2);
+			this.grpPizarra2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarra2.ForeColor = System.Drawing.Color.White;
+			this.grpPizarra2.Location = new System.Drawing.Point(254, 129);
+			this.grpPizarra2.Name = "grpPizarra2";
+			this.grpPizarra2.Size = new System.Drawing.Size(218, 475);
+			this.grpPizarra2.TabIndex = 0;
+			this.grpPizarra2.TabStop = false;
 			// 
 			// lblResultado2
 			// 
@@ -118,23 +128,98 @@
 			this.lblResultado2.TabIndex = 0;
 			this.lblResultado2.Text = "... sigue";
 			// 
+			// grpPizarra4
+			// 
+			this.grpPizarra4.Controls.Add(this.lblResultado4);
+			this.grpPizarra4.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarra4.ForeColor = System.Drawing.Color.White;
+			this.grpPizarra4.Location = new System.Drawing.Point(717, 129);
+			this.grpPizarra4.Name = "grpPizarra4";
+			this.grpPizarra4.Size = new System.Drawing.Size(218, 475);
+			this.grpPizarra4.TabIndex = 0;
+			this.grpPizarra4.TabStop = false;
+			// 
+			// lblResultado4
+			// 
+			this.lblResultado4.AutoSize = true;
+			this.lblResultado4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado4.ForeColor = System.Drawing.Color.White;
+			this.lblResultado4.Location = new System.Drawing.Point(17, 29);
+			this.lblResultado4.Name = "lblResultado4";
+			this.lblResultado4.Size = new System.Drawing.Size(72, 20);
+			this.lblResultado4.TabIndex = 0;
+			this.lblResultado4.Text = "... sigue";
+			// 
+			// grpPizarra3
+			// 
+			this.grpPizarra3.Controls.Add(this.lblResultado3);
+			this.grpPizarra3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarra3.ForeColor = System.Drawing.Color.White;
+			this.grpPizarra3.Location = new System.Drawing.Point(486, 129);
+			this.grpPizarra3.Name = "grpPizarra3";
+			this.grpPizarra3.Size = new System.Drawing.Size(218, 475);
+			this.grpPizarra3.TabIndex = 0;
+			this.grpPizarra3.TabStop = false;
+			// 
+			// lblResultado3
+			// 
+			this.lblResultado3.AutoSize = true;
+			this.lblResultado3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado3.ForeColor = System.Drawing.Color.White;
+			this.lblResultado3.Location = new System.Drawing.Point(17, 29);
+			this.lblResultado3.Name = "lblResultado3";
+			this.lblResultado3.Size = new System.Drawing.Size(72, 20);
+			this.lblResultado3.TabIndex = 0;
+			this.lblResultado3.Text = "... sigue";
+			// 
+			// grpPizarra5
+			// 
+			this.grpPizarra5.Controls.Add(this.lblResultado5);
+			this.grpPizarra5.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grpPizarra5.ForeColor = System.Drawing.Color.White;
+			this.grpPizarra5.Location = new System.Drawing.Point(947, 129);
+			this.grpPizarra5.Name = "grpPizarra5";
+			this.grpPizarra5.Size = new System.Drawing.Size(218, 475);
+			this.grpPizarra5.TabIndex = 0;
+			this.grpPizarra5.TabStop = false;
+			// 
+			// lblResultado5
+			// 
+			this.lblResultado5.AutoSize = true;
+			this.lblResultado5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado5.ForeColor = System.Drawing.Color.White;
+			this.lblResultado5.Location = new System.Drawing.Point(17, 29);
+			this.lblResultado5.Name = "lblResultado5";
+			this.lblResultado5.Size = new System.Drawing.Size(72, 20);
+			this.lblResultado5.TabIndex = 0;
+			this.lblResultado5.Text = "... sigue";
+			// 
 			// Prog_Tema_3_Ej_25
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Black;
-			this.ClientSize = new System.Drawing.Size(484, 604);
-			this.Controls.Add(this.grpPizarraDcha);
+			this.ClientSize = new System.Drawing.Size(1177, 625);
+			this.Controls.Add(this.grpPizarra5);
+			this.Controls.Add(this.grpPizarra4);
+			this.Controls.Add(this.grpPizarra3);
+			this.Controls.Add(this.grpPizarra2);
 			this.Controls.Add(this.lblSubtitulo);
-			this.Controls.Add(this.grpPizarraIzq);
+			this.Controls.Add(this.grpPizarra1);
 			this.Controls.Add(this.btnMostrar);
 			this.Controls.Add(this.lblTitulo);
 			this.Name = "Prog_Tema_3_Ej_25";
 			this.Text = "Prog_Tema_3_Ej_25";
-			this.grpPizarraIzq.ResumeLayout(false);
-			this.grpPizarraIzq.PerformLayout();
-			this.grpPizarraDcha.ResumeLayout(false);
-			this.grpPizarraDcha.PerformLayout();
+			this.grpPizarra1.ResumeLayout(false);
+			this.grpPizarra1.PerformLayout();
+			this.grpPizarra2.ResumeLayout(false);
+			this.grpPizarra2.PerformLayout();
+			this.grpPizarra4.ResumeLayout(false);
+			this.grpPizarra4.PerformLayout();
+			this.grpPizarra3.ResumeLayout(false);
+			this.grpPizarra3.PerformLayout();
+			this.grpPizarra5.ResumeLayout(false);
+			this.grpPizarra5.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -145,9 +230,15 @@
 		private System.Windows.Forms.Label lblTitulo;
 		private System.Windows.Forms.Button btnMostrar;
 		private System.Windows.Forms.Label lblResultado1;
-		private System.Windows.Forms.GroupBox grpPizarraIzq;
+		private System.Windows.Forms.GroupBox grpPizarra1;
 		private System.Windows.Forms.Label lblSubtitulo;
-		private System.Windows.Forms.GroupBox grpPizarraDcha;
+		private System.Windows.Forms.GroupBox grpPizarra2;
 		private System.Windows.Forms.Label lblResultado2;
+		private System.Windows.Forms.GroupBox grpPizarra4;
+		private System.Windows.Forms.Label lblResultado4;
+		private System.Windows.Forms.GroupBox grpPizarra3;
+		private System.Windows.Forms.Label lblResultado3;
+		private System.Windows.Forms.GroupBox grpPizarra5;
+		private System.Windows.Forms.Label lblResultado5;
 		}
 	}
--- Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.cs
+++ Tema_03/Tema_03_Ejercicio_25/Prog_Tema_3_Ej_25/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,61 @@ namespace Prog_Tema_3_Ej_25
 			{
 			InitializeComponent();
 			}
+
+		private void btnMostrar_Click(object sender, EventArgs e)
+			{
+			lblResultado1.Text = "";
+			lblResultado2.Text = "";
+			lblResultado3.Text = "";
+			lblResultado4.Text = "";
+			lblResultado5.Text = "";
+			int contador = 1;
+			int contadorBucle = 0;
+			string tablaEnCurso = "";
+			while (contador < 11) 
+			{ 
+				// El bucle for escribe la tabla de multiplicar y la almacena en la variable 'tablaEnCurso'
+				for (int i = 1; i < 11; i++)
+					{
+					tablaEnCurso += $"{contador} * {i} = {contador * i}\n";
+					}
+				tablaEnCurso += "\n"; // se añade un salto de línea a 'tablaEnCurso' para separar las tablas de multiplicar
+
+				// Se incrementa la variable 'contadorBucle' para saber en qué tabla de multiplicar estamos y así
+				// cambiar el label de salida: cada dos tablas completas, se mueve al label de la derecha
+				contadorBucle++;
+
+				// Estos bloques condicionales evalúan el valor de 'contadorBucle' y señalan el label de salida
+				if ((contador > 0) && (contador <= 2))
+					{
+					lblResultado1.Text += tablaEnCurso;
+					}
+				else if ((contador > 2) && (contador <= 4))
+					{
+					lblResultado2.Text += tablaEnCurso;
+					}
+				else if ((contador > 4) && (contador <= 6))
+					{
+					lblResultado3.Text += tablaEnCurso;
+					}
+				else if ((contador > 6) && (contador <= 8))
+					{
+					lblResultado4.Text += tablaEnCurso;
+					}
+				else
+					{
+					lblResultado5.Text += tablaEnCurso;
+					}
+
+				// Se borra el contenido de la variable 'tablaEnCurso' para que la vuelva a rellenar el bucle for
+				// con el nuevo valor de 'contador' que se incrementa en la línea siguiente
+				tablaEnCurso = "";
+				contador++; // Se repetirá todo hasta que 'contador' alcance el valor de 11
+			}
+
+			
+
+			}
+			
 		}
 	}
