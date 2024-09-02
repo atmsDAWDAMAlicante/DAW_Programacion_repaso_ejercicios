353fd6a6fff620172499d5625c8bd8b7bd5b6431
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-02 16:54:04 +0200

    Diseño del Form

--- Tema_04/Tema_04_Ejercicio_01/Prog_Tema_4_Ej_01/Form1.Designer.cs
+++ Tema_04/Tema_04_Ejercicio_01/Prog_Tema_4_Ej_01/Form1.Designer.cs
@@ -28,58 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNumero1 = new System.Windows.Forms.TextBox();
-            this.txtNumero2 = new System.Windows.Forms.TextBox();
-            this.btnSuma = new System.Windows.Forms.Button();
-            this.lblResultado = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // txtNumero1
-            // 
-            this.txtNumero1.Location = new System.Drawing.Point(16, 12);
-            this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(105, 26);
-            this.txtNumero1.TabIndex = 0;
-            // 
-            // txtNumero2
-            // 
-            this.txtNumero2.Location = new System.Drawing.Point(16, 105);
-            this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(105, 26);
-            this.txtNumero2.TabIndex = 1;
-            // 
-            // btnSuma
-            // 
-            this.btnSuma.Location = new System.Drawing.Point(43, 56);
-            this.btnSuma.Name = "btnSuma";
-            this.btnSuma.Size = new System.Drawing.Size(52, 34);
-            this.btnSuma.TabIndex = 2;
-            this.btnSuma.Text = "+";
-            this.btnSuma.UseVisualStyleBackColor = true;
-            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(59, 146);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(21, 20);
-            this.lblResultado.TabIndex = 3;
-            this.lblResultado.Text = "...";
-            // 
-            // Prog_Tema_4_Ej_01
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(176, 189);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.btnSuma);
-            this.Controls.Add(this.txtNumero2);
-            this.Controls.Add(this.txtNumero1);
-            this.Name = "Prog_Tema_4_Ej_01";
-            this.Text = "Prog_Tema_4_Ej_01";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtNumero1 = new System.Windows.Forms.TextBox();
+			this.txtNumero2 = new System.Windows.Forms.TextBox();
+			this.btnSuma = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtNumero1
+			// 
+			this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNumero1.ForeColor = System.Drawing.Color.CornflowerBlue;
+			this.txtNumero1.Location = new System.Drawing.Point(76, 89);
+			this.txtNumero1.Name = "txtNumero1";
+			this.txtNumero1.Size = new System.Drawing.Size(50, 30);
+			this.txtNumero1.TabIndex = 0;
+			// 
+			// txtNumero2
+			// 
+			this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNumero2.ForeColor = System.Drawing.Color.CornflowerBlue;
+			this.txtNumero2.Location = new System.Drawing.Point(211, 88);
+			this.txtNumero2.Name = "txtNumero2";
+			this.txtNumero2.Size = new System.Drawing.Size(50, 30);
+			this.txtNumero2.TabIndex = 1;
+			// 
+			// btnSuma
+			// 
+			this.btnSuma.BackColor = System.Drawing.Color.MediumBlue;
+			this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSuma.ForeColor = System.Drawing.Color.White;
+			this.btnSuma.Location = new System.Drawing.Point(141, 83);
+			this.btnSuma.Name = "btnSuma";
+			this.btnSuma.Size = new System.Drawing.Size(55, 45);
+			this.btnSuma.TabIndex = 2;
+			this.btnSuma.Text = "+";
+			this.btnSuma.UseVisualStyleBackColor = false;
+			this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.ForeColor = System.Drawing.Color.White;
+			this.lblResultado.Location = new System.Drawing.Point(72, 146);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(138, 29);
+			this.lblResultado.TabIndex = 3;
+			this.lblResultado.Text = "Resultado:";
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.White;
+			this.lblTitulo.Location = new System.Drawing.Point(63, 17);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(222, 55);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "ƒ suma()";
+			// 
+			// Prog_Tema_4_Ej_01
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.CornflowerBlue;
+			this.ClientSize = new System.Drawing.Size(358, 209);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnSuma);
+			this.Controls.Add(this.txtNumero2);
+			this.Controls.Add(this.txtNumero1);
+			this.Name = "Prog_Tema_4_Ej_01";
+			this.Text = "Prog_Tema_4_Ej_01";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -89,6 +112,7 @@
         private System.Windows.Forms.TextBox txtNumero2;
         private System.Windows.Forms.Button btnSuma;
         private System.Windows.Forms.Label lblResultado;
-    }
+		private System.Windows.Forms.Label lblTitulo;
+		}
 }
 
