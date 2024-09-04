bba7141ab5b728f280d523a058a7f7f2cf350123
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-04 20:20:21 +0200

    Diseño del Form

--- Tema_04/Tema_04_Ejercicio_06/Prog_Tema_4_Ej_06/Form1.Designer.cs
+++ Tema_04/Tema_04_Ejercicio_06/Prog_Tema_4_Ej_06/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Prog_Tema_4_Ej_06
+﻿namespace Prog_Tema_4_Ej_06_
 {
     partial class Prog_Tema_4_Ej_06
     {
@@ -28,103 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNumero1 = new System.Windows.Forms.TextBox();
-            this.txtNumero2 = new System.Windows.Forms.TextBox();
-            this.btnAccion = new System.Windows.Forms.Button();
-            this.lblTitulo1 = new System.Windows.Forms.Label();
-            this.lblTitulo2 = new System.Windows.Forms.Label();
-            this.lblResultadoDivision = new System.Windows.Forms.Label();
-            this.lblResto = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // txtNumero1
-            // 
-            this.txtNumero1.Location = new System.Drawing.Point(27, 18);
-            this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(99, 26);
-            this.txtNumero1.TabIndex = 0;
-            // 
-            // txtNumero2
-            // 
-            this.txtNumero2.Location = new System.Drawing.Point(27, 68);
-            this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(99, 26);
-            this.txtNumero2.TabIndex = 1;
-            // 
-            // btnAccion
-            // 
-            this.btnAccion.Location = new System.Drawing.Point(27, 118);
-            this.btnAccion.Name = "btnAccion";
-            this.btnAccion.Size = new System.Drawing.Size(99, 29);
-            this.btnAccion.TabIndex = 2;
-            this.btnAccion.Text = "Acción";
-            this.btnAccion.UseVisualStyleBackColor = true;
-            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
-            // 
-            // lblTitulo1
-            // 
-            this.lblTitulo1.AutoSize = true;
-            this.lblTitulo1.Location = new System.Drawing.Point(31, 179);
-            this.lblTitulo1.Name = "lblTitulo1";
-            this.lblTitulo1.Size = new System.Drawing.Size(135, 40);
-            this.lblTitulo1.TabIndex = 3;
-            this.lblTitulo1.Text = "El resultado de la\ndivisión entera es:";
-            // 
-            // lblTitulo2
-            // 
-            this.lblTitulo2.AutoSize = true;
-            this.lblTitulo2.Location = new System.Drawing.Point(31, 270);
-            this.lblTitulo2.Name = "lblTitulo2";
-            this.lblTitulo2.Size = new System.Drawing.Size(88, 20);
-            this.lblTitulo2.TabIndex = 4;
-            this.lblTitulo2.Text = "El resto es:";
-            // 
-            // lblResultadoDivision
-            // 
-            this.lblResultadoDivision.AutoSize = true;
-            this.lblResultadoDivision.Location = new System.Drawing.Point(31, 230);
-            this.lblResultadoDivision.Name = "lblResultadoDivision";
-            this.lblResultadoDivision.Size = new System.Drawing.Size(25, 24);
-            this.lblResultadoDivision.TabIndex = 5;
-            this.lblResultadoDivision.Text = "...";
-            // 
-            // lblResto
-            // 
-            this.lblResto.AutoSize = true;
-            this.lblResto.Location = new System.Drawing.Point(31, 311);
-            this.lblResto.Name = "lblResto";
-            this.lblResto.Size = new System.Drawing.Size(25, 24);
-            this.lblResto.TabIndex = 6;
-            this.lblResto.Text = "...";
-            // 
-            // Prog_Tema_4_Ej_06
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(193, 352);
-            this.Controls.Add(this.lblResto);
-            this.Controls.Add(this.lblResultadoDivision);
-            this.Controls.Add(this.lblTitulo2);
-            this.Controls.Add(this.lblTitulo1);
-            this.Controls.Add(this.btnAccion);
-            this.Controls.Add(this.txtNumero2);
-            this.Controls.Add(this.txtNumero1);
-            this.Name = "Prog_Tema_4_Ej_06";
-            this.Text = "Prog_Tema_4_Ej_06";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.txtNumero1 = new System.Windows.Forms.TextBox();
+			this.txtNumero2 = new System.Windows.Forms.TextBox();
+			this.btnAccion = new System.Windows.Forms.Button();
+			this.lblTitulo1 = new System.Windows.Forms.Label();
+			this.lblTitulo2 = new System.Windows.Forms.Label();
+			this.lblResultadoDivision = new System.Windows.Forms.Label();
+			this.lblResto = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.ForeColor = System.Drawing.Color.Navy;
+			this.lblTitulo.Location = new System.Drawing.Point(19, 25);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(428, 40);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "ƒ División entera y resto";
+			// 
+			// txtNumero1
+			// 
+			this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNumero1.ForeColor = System.Drawing.Color.Navy;
+			this.txtNumero1.Location = new System.Drawing.Point(31, 90);
+			this.txtNumero1.Name = "txtNumero1";
+			this.txtNumero1.Size = new System.Drawing.Size(119, 35);
+			this.txtNumero1.TabIndex = 1;
+			// 
+			// txtNumero2
+			// 
+			this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNumero2.ForeColor = System.Drawing.Color.Navy;
+			this.txtNumero2.Location = new System.Drawing.Point(328, 90);
+			this.txtNumero2.Name = "txtNumero2";
+			this.txtNumero2.Size = new System.Drawing.Size(119, 35);
+			this.txtNumero2.TabIndex = 2;
+			// 
+			// btnAccion
+			// 
+			this.btnAccion.BackColor = System.Drawing.Color.Navy;
+			this.btnAccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAccion.ForeColor = System.Drawing.Color.White;
+			this.btnAccion.Location = new System.Drawing.Point(175, 84);
+			this.btnAccion.Name = "btnAccion";
+			this.btnAccion.Size = new System.Drawing.Size(129, 52);
+			this.btnAccion.TabIndex = 3;
+			this.btnAccion.Text = "Acción";
+			this.btnAccion.UseVisualStyleBackColor = false;
+			this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+			// 
+			// lblTitulo1
+			// 
+			this.lblTitulo1.AutoSize = true;
+			this.lblTitulo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo1.ForeColor = System.Drawing.Color.Navy;
+			this.lblTitulo1.Location = new System.Drawing.Point(27, 175);
+			this.lblTitulo1.Name = "lblTitulo1";
+			this.lblTitulo1.Size = new System.Drawing.Size(248, 70);
+			this.lblTitulo1.TabIndex = 0;
+			this.lblTitulo1.Text = "El resultado de la\ndivisión entera es:";
+			// 
+			// lblTitulo2
+			// 
+			this.lblTitulo2.AutoSize = true;
+			this.lblTitulo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo2.ForeColor = System.Drawing.Color.Navy;
+			this.lblTitulo2.Location = new System.Drawing.Point(27, 293);
+			this.lblTitulo2.Name = "lblTitulo2";
+			this.lblTitulo2.Size = new System.Drawing.Size(160, 35);
+			this.lblTitulo2.TabIndex = 0;
+			this.lblTitulo2.Text = "El resto es:";
+			// 
+			// lblResultadoDivision
+			// 
+			this.lblResultadoDivision.AutoSize = true;
+			this.lblResultadoDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultadoDivision.ForeColor = System.Drawing.Color.Navy;
+			this.lblResultadoDivision.Location = new System.Drawing.Point(260, 205);
+			this.lblResultadoDivision.Name = "lblResultadoDivision";
+			this.lblResultadoDivision.Size = new System.Drawing.Size(31, 29);
+			this.lblResultadoDivision.TabIndex = 0;
+			this.lblResultadoDivision.Text = "...";
+			// 
+			// lblResto
+			// 
+			this.lblResto.AutoSize = true;
+			this.lblResto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResto.ForeColor = System.Drawing.Color.Navy;
+			this.lblResto.Location = new System.Drawing.Point(183, 294);
+			this.lblResto.Name = "lblResto";
+			this.lblResto.Size = new System.Drawing.Size(31, 29);
+			this.lblResto.TabIndex = 0;
+			this.lblResto.Text = "...";
+			// 
+			// Prog_Tema_4_Ej_06_
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.ClientSize = new System.Drawing.Size(506, 369);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.lblResto);
+			this.Controls.Add(this.lblResultadoDivision);
+			this.Controls.Add(this.lblTitulo2);
+			this.Controls.Add(this.lblTitulo1);
+			this.Controls.Add(this.btnAccion);
+			this.Controls.Add(this.txtNumero2);
+			this.Controls.Add(this.txtNumero1);
+			this.Name = "Prog_Tema_4_Ej_06";
+			this.Text = "Prog_Tema_4_Ej_06";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.TextBox txtNumero1;
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.TextBox txtNumero1;
         private System.Windows.Forms.TextBox txtNumero2;
         private System.Windows.Forms.Button btnAccion;
         private System.Windows.Forms.Label lblTitulo1;
         private System.Windows.Forms.Label lblTitulo2;
         private System.Windows.Forms.Label lblResultadoDivision;
         private System.Windows.Forms.Label lblResto;
-    }
+		}
 }
 
