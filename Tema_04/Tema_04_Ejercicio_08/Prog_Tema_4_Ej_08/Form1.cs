08a1964d04eadd9840c815b626d25ecaecb0ed46
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-06 16:24:47 +0200

    Programa completo

--- Tema_04/Tema_04_Ejercicio_08/Prog_Tema_4_Ej_08/Form1.cs
+++ Tema_04/Tema_04_Ejercicio_08/Prog_Tema_4_Ej_08/Form1.cs
@@ -19,45 +19,44 @@ namespace Prog_Tema_4_Ej_08_
 
         private void btnAccion_Click(object sender, EventArgs e)
         {
-            double num1 = 0, num2 = 0, num3 = 0;
-            ValidarCampos1(ref num1, ref num2, ref num3);
-        }
+            // El label imprimirá por pantalla el resultado de la función ValidarCampos() que, devuelve un string
+            lblResultado.Text = ValidarCampos();
+		}
 
 
-        public void ValidarCampos1(ref double num1, ref double num2, ref double num3)
+        public string ValidarCampos()
         {
-            bool num1esnum = double.TryParse(txtNum1.Text, out num1);
-            bool num2esnum = double.TryParse(txtNum2.Text, out num2);
-            bool num3esnum = double.TryParse(txtNum3.Text, out num3);
+            bool num1esnum = double.TryParse(txtNum1.Text, out double num1);
+            bool num2esnum = double.TryParse(txtNum2.Text, out double num2);
+            bool num3esnum = double.TryParse(txtNum3.Text, out double num3);
 
             if (num1esnum && num2esnum && num3esnum)
             {
-                ValidarCampos2(ref num1, ref num2, ref num3);
+				if ((num1 >= 0 && num1 < 11) && (num2 >= 0 && num2 < 11) && (num3 >= 0 && num3 < 11))
+					{
+					// Si los tres números introducidos son números válidos se envían a la función CalcularLaMedia
+                    // que devolverá la media calculada y, una vez recibidos, se devuelven al evento click del botón
+                    // que lo imprime por pantalla
+					return CalcularLaMedia(num1, num2, num3);
+					}
+				else
+					{
+					return "Introduce un valor\nentre 0 y 10";
+					}
             }
             else 
             {
-                lblResultado.Text = "Introduce números";
+                return "Introduce números.";
             }
         }
 
-        public void ValidarCampos2(ref double num1, ref double num2, ref double num3)
-        {
-            if ((num1 >= 0 && num1 < 11) && (num2 >= 0 && num2 < 11) && (num3 >= 0 && num3 < 11))
-            {
-                CalcularLaMedia(ref num1, ref num2, ref num3);
-            }
-            else 
-            {
-				lblResultado.Text = "Introduce un valor\nentre 0 y 10";
-            }
-        }
 
-        public void CalcularLaMedia(ref double num1, ref double num2, ref double num3)
+        public string CalcularLaMedia(double num1, double num2, double num3)
         {
             double media = (( num1 + num2 + num3 ) / 3 );
-            // lblResultado.Text = "La nota media\nes " + Math.Round(media, 2).ToString("#.##");
-            lblResultado.Text = $"{media:F2}";
+            return $"{media:F2}"; // :F2 formatea el double media con dos decimales
         }
+        
     }
 
 }
