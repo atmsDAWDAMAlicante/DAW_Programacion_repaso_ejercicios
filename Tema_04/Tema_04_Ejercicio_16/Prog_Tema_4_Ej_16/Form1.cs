79fec265aa3d8f984c69eb8f2c6f2ddb104caad1
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-07 17:01:00 +0200

    Diseño del Form

--- Tema_04/Tema_04_Ejercicio_16/Prog_Tema_4_Ej_16/Form1.cs
+++ Tema_04/Tema_04_Ejercicio_16/Prog_Tema_4_Ej_16/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Prog_Tema_4_Ej_16
+namespace Prog_Tema_4_Ej_16_
 	{
 	public partial class Prog_Tema_4_Ej_16 : Form
 		{
