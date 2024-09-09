0a37778c75a4e5431025d851560ddd545c78fe7d
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-09 20:18:33 +0200

    Diseño del Form

--- Tema_05/Tema_05_Ejercicio_01/Prog_Tema_5_Ej_01/Form1.Designer.cs
+++ Tema_05/Tema_05_Ejercicio_01/Prog_Tema_5_Ej_01/Form1.Designer.cs
@@ -28,20 +28,73 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.lblSubtitulo = new System.Windows.Forms.Label();
+			this.btnMostrar = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitulo.Location = new System.Drawing.Point(0, 9);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(428, 46);
+			this.lblTitulo.TabIndex = 0;
+			this.lblTitulo.Text = "⋎ Lectura de un Vector";
+			// 
+			// lblSubtitulo
+			// 
+			this.lblSubtitulo.AutoSize = true;
+			this.lblSubtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubtitulo.Location = new System.Drawing.Point(58, 68);
+			this.lblSubtitulo.Name = "lblSubtitulo";
+			this.lblSubtitulo.Size = new System.Drawing.Size(330, 50);
+			this.lblSubtitulo.TabIndex = 0;
+			this.lblSubtitulo.Text = "Los siguientes números se muestran\nleyéndolos de un vector:";
+			// 
+			// btnMostrar
+			// 
+			this.btnMostrar.Location = new System.Drawing.Point(136, 139);
+			this.btnMostrar.Name = "btnMostrar";
+			this.btnMostrar.Size = new System.Drawing.Size(119, 46);
+			this.btnMostrar.TabIndex = 1;
+			this.btnMostrar.Text = "Mostrar";
+			this.btnMostrar.UseVisualStyleBackColor = true;
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(58, 229);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(99, 25);
+			this.lblResultado.TabIndex = 0;
+			this.lblResultado.Text = "Resultado";
 			// 
 			// Prog_Tema_5_Ej_01
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(501, 365);
+			this.Controls.Add(this.lblSubtitulo);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnMostrar);
+			this.Controls.Add(this.lblTitulo);
 			this.Name = "Prog_Tema_5_Ej_01";
 			this.Text = "Prog_Tema_5_Ej_01";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label lblSubtitulo;
+		private System.Windows.Forms.Button btnMostrar;
+		private System.Windows.Forms.Label lblResultado;
 		}
 	}
 
