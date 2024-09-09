61abd8a01f18a4936519ee07baeaaa7133ab333a
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-09 16:58:31 +0200

    Programa completo

--- Tema_04/Tema_04_Ejercicio_11/Prog_Tema_4_Ej_11/Form1.cs
+++ Tema_04/Tema_04_Ejercicio_11/Prog_Tema_4_Ej_11/Form1.cs
@@ -25,23 +25,30 @@ namespace Prog_Tema_4_Ej_11_
 
 			if (numEsNum)
 				{
-				lblResultado.Text = EsBisiesto(yearIntroducido);
+				lblResultado.Text = EsBisiesto(yearIntroducido); // la función devuelve un string para evaluarlo en la prueba unitaria
 				}
 			else
 				{
 				lblResultado.Text = "Introduce números.";
 				}
-
 			}
 
 		public string EsBisiesto(int yearIntroducido)
 			{
-
-
-			return $"{yearIntroducido}";
+			// Con arreglo a la formulación literal del enunciado:
+			// "Los Años bisiestos son los divisibles por 4 excepto los divisibles por 100 y no divisibles por 400."
+			// Desgranando dicho enunciado:
+			// a) "Los Años bisiestos son los divisibles por 4" equivale a yearIntroducido%4==0
+			// b) "excepto los divisibles por 100" equivale a !(yearIntroducido%100==0) Atención a la negación ! que es el 'excepto'
+			// c) "y no divisibles por 400" equivale a  yearIntroducido%400!=0
+			if ( (yearIntroducido%4==0) && !(yearIntroducido%100==0) && (yearIntroducido%400!=0) )
+				{
+				return $"{yearIntroducido} SÍ es bisiesto.";
+				}
+			else
+				{
+				return $"{yearIntroducido} NO es bisiesto.";
+				}
 			}
-
-
-
 		}
 	}
