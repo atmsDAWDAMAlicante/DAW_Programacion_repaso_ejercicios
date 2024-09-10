7dee173cbd9bbabbbd06880d22a208a3d538c8fd
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-10 13:28:54 +0200

    Comentarios añadidos al código del programa

--- Tema_04/Tema_04_Ejercicio_12/Prog_Tema_4_Ej_12/Form1.cs
+++ Tema_04/Tema_04_Ejercicio_12/Prog_Tema_4_Ej_12/Form1.cs
@@ -26,12 +26,21 @@ namespace Prog_Tema_4_Ej_12_
 			bool yearEsNum = int.TryParse(txtYear.Text, out int yearNum);
 			bool fechaValidacion, bisiesto;
 
+			// Primero, se evalúa si lo introducido en los TextBox son números
 			if (diaEsNum && mesEsNum && yearEsNum) 
 				{
+				// Segundo, se evalúa si el número correspondiente al año tiene cuatro dígitos
 				if ((yearNum.ToString().Length == 4))
-					{ 
+					{
+					// Tercero, se evalúa si el año es bisiesto llamando a la función 'EsBisiesto' y pasándole el
+					// valor de la variable que almacena el año. El resultado lo recoge una variable booleana
 					bisiesto = EsBisiesto(yearNum);
+					// En cuarto lugar, se valida la fecha enviando, los tres enteros que componen la fecha, a
+					// una función VerificarFecha() que devuelve un valor booleano que se recoge en 'fechaValidacion'
 					fechaValidacion = VerificarFecha(diaNum, mesNum, bisiesto);
+					// Si 'fechaValidacion' es true, se envía el entero diaNum a la función ComponerFecha() para que le
+					// agregue un '0' delante si el número es inferior a 10; y, a continuación, se hace lo mismo con el 
+					// valor de mesNum, para finalmente obtener una fecha del tipo '03/09/1977' o '02/04/2016'
 					if (fechaValidacion)
 						{
 						lblResultado.Text = $"{ComponerFecha(diaNum.ToString())}/{ComponerFecha(mesNum.ToString())}/{yearNum}";
@@ -101,11 +110,5 @@ namespace Prog_Tema_4_Ej_12_
 				return $"{diaOmes}";
 				}
 			}
-
-
-
-
-
-
 		}
 	}
