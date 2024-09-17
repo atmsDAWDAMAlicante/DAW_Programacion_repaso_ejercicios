aad0f04ebf512129f94659eea5a3b5b592b60c4a
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-17 18:03:28 +0200

    Añadido proyecto de prueba unitaria

---
+++ Tema_04/Tema_04_Ejercicio_14/Tests_Tema_4_Ej_14/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests_Tema_4_Ej_14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests_Tema_4_Ej_14")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ff1ce7c-9768-4e15-869a-1117e942ee37")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tema_04/Tema_04_Ejercicio_14/Tests_Tema_4_Ej_14/Tests_Tema_4_Ej_14.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+using Prog_Tema_4_Ej_14_;
+
+namespace Tests_Tema_4_Ej_14
+	{
+	[TestClass]
+	public class Tests_Tema_4_Ej_14
+		{
+		[TestMethod]
+		[DataRow(10500,10000, 1)]
+		[DataRow(7000,2000, 1)]
+		[DataRow(4000,2000, 2)]
+		public void TestCambioUnidades(int cantidadTotal, int fraccion, int expected)
+			{
+
+			// Act
+			Prog_Tema_4_Ej_14 objetoPrograma = new Prog_Tema_4_Ej_14();
+			int actual = objetoPrograma.CalcularUnidades(cantidadTotal, fraccion);
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+
+			}
+		}
+	}
