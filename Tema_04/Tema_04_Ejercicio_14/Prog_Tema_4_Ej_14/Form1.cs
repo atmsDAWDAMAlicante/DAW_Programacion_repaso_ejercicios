c101074293ecce096f67f4d916e1fb21f0f04b74
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-17 18:42:11 +0200

    Programa y prueba unitaria completos

--- Tema_04/Tema_04_Ejercicio_14/Prog_Tema_4_Ej_14/Form1.cs
+++ Tema_04/Tema_04_Ejercicio_14/Prog_Tema_4_Ej_14/Form1.cs
@@ -19,33 +19,91 @@ namespace Prog_Tema_4_Ej_14_
 
 		private void btnSuCambio_Click(object sender, EventArgs e)
 			{
+			// Con la cantidad 18888 salen todos los billetes y monedas
+
 			lblResultado.Text = "";
-			int fraccion = 10000;
+			int unidades;
 			bool entradaEsNum = int.TryParse(txtCantidad.Text, out int cantidad);
-			if (entradaEsNum)
+
+			if ((entradaEsNum) && (cantidad > 0))
 				{
-				do
-					{
-					if (cantidad > fraccion)
-						{
-						ImprimirResultado((cantidad - fraccion).ToString());
-						}
-					cantidad -= 5000;
-					} while (cantidad > 0);
+				// Cada dos líneas se llaman a dos funciones. 
+				// 'CalcularUnidades' devuelve el número de unidades del tipo que corresponde a la cantidad que se le pasa
+				// y reduce el valor de la variable 'cantidad' que se le pasa por referencia
+
+				// La variable 'ImprimirResultado' recibe la unidad y la cuantía de cada billete o moneda y va concatenando el resultado que imprime
+				unidades = CalcularUnidades(ref cantidad, 10000);
+				ImprimirResultado(unidades, 10000);
+				unidades = CalcularUnidades(ref cantidad, 5000);
+				ImprimirResultado(unidades, 5000);
+				unidades = CalcularUnidades(ref cantidad, 2000);
+				ImprimirResultado(unidades, 2000);
+				unidades = CalcularUnidades(ref cantidad, 1000);
+				ImprimirResultado(unidades, 1000);
+				unidades = CalcularUnidades(ref cantidad, 500);
+				ImprimirResultado(unidades, 500);
+				unidades = CalcularUnidades(ref cantidad, 200);
+				ImprimirResultado(unidades, 200);
+				unidades = CalcularUnidades(ref cantidad, 100);
+				ImprimirResultado(unidades, 100);
+				unidades = CalcularUnidades(ref cantidad, 50);
+				ImprimirResultado(unidades, 50);
+				unidades = CalcularUnidades(ref cantidad, 25);
+				ImprimirResultado(unidades, 25);
+				unidades = CalcularUnidades(ref cantidad, 10);
+				ImprimirResultado(unidades, 10);
+				unidades = CalcularUnidades(ref cantidad, 5);
+				ImprimirResultado(unidades, 5);
+				unidades = CalcularUnidades(ref cantidad, 2);
+				ImprimirResultado(unidades, 2);
+				unidades = CalcularUnidades(ref cantidad, 1);
+				ImprimirResultado(unidades, 1);
 				}
 			else
 				{
-				lblResultado.Text = "Introduzca números.";
+				lblResultado.Text = "Introduzca números posiivos.";
 				}
 			}
 
-		public int CalcularUnidades(int cantidad, int fraccion)
+		public int CalcularUnidades(ref int cantidad, int fraccion)
 			{
-			return 1;
+			// Una variable recoge las unidades resultantes para cada cuantía que se pasa a la función
+			int unidades = cantidad / fraccion;
+			// A continuación se actualiza la variable 'cantidad' que se ha pasado por referencia
+			cantidad -= (fraccion * unidades);
+			// Finalmente se retorna las unidades resultantes
+			return unidades;
 			}
-		public void ImprimirResultado(string nuevaLinea)
+		public void ImprimirResultado(int unidades, int fraccion)
 			{
-			lblResultado.Text += $"{nuevaLinea}\n";
+				string tipo = "billete";
+				string plural = "";
+
+				// Sólo si hay unidades de una cuantía determinada, se imprimirá una línea para esa cuantía
+				if (unidades != 0)
+				{
+					// Con este bloque condicional se imprime en plural o singular las palabras 'billete' o 'moneda'
+					if (unidades > 1)
+					{ 
+						plural = "s";
+					}
+					else
+					{ 
+						plural = "";
+					}
+
+					// Con este bloque condicional se imprime la palabras 'billete' o 'moneda'
+					if (fraccion >= 1000)
+					{ 
+						tipo = "billete";
+					}
+					else
+					{ 
+						tipo = "moneda";
+					}
+				// Aquí se van concatenando todas las cadenas, cada vez que se llama a la función y las unidades son > 0
+				lblResultado.Text += $"{unidades} {tipo}{plural} de {fraccion} pesetas.\n";
+				}
 			}
 
 
--- Tema_04/Tema_04_Ejercicio_14/Tests_Tema_4_Ej_14/Tests_Tema_4_Ej_14.cs
+++ Tema_04/Tema_04_Ejercicio_14/Tests_Tema_4_Ej_14/Tests_Tema_4_Ej_14.cs
@@ -10,14 +10,17 @@ namespace Tests_Tema_4_Ej_14
 		{
 		[TestMethod]
 		[DataRow(10500,10000, 1)]
-		[DataRow(7000,2000, 1)]
+		[DataRow(7000,5000, 1)]
 		[DataRow(4000,2000, 2)]
+		[DataRow(1000, 500, 2)]
+		[DataRow(17, 5, 3)]
+		[DataRow(1, 1, 1)]
 		public void TestCambioUnidades(int cantidadTotal, int fraccion, int expected)
 			{
 
 			// Act
 			Prog_Tema_4_Ej_14 objetoPrograma = new Prog_Tema_4_Ej_14();
-			int actual = objetoPrograma.CalcularUnidades(cantidadTotal, fraccion);
+			int actual = objetoPrograma.CalcularUnidades(ref cantidadTotal, fraccion);
 
 			// Assert
 			Assert.AreEqual(expected, actual);
