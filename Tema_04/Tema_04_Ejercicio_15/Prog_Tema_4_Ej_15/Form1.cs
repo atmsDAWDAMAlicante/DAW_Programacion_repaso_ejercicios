d701a0bd8dc3cf8f259101c939a1b4b0a550c7be
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-18 19:54:49 +0200

    Programa y prueba unitaria completos

--- Tema_04/Tema_04_Ejercicio_15/Prog_Tema_4_Ej_15/Form1.cs
+++ Tema_04/Tema_04_Ejercicio_15/Prog_Tema_4_Ej_15/Form1.cs
@@ -21,19 +21,135 @@ namespace Prog_Tema_4_Ej_15_
 			{
 			lblResultado.Text = "";
 
+			// Declaración de las variables
+			int unidades; // La variable 'unidades' irá recogiendo el número de billetes o monedas por fracción
+			double centimos; // La variable 'centimos' servirá para descontar la cantidad una vez baje de 1 € de importe
+			string centimosEuros = "€"; // Esta variable 'centimosEuros' modifica el string "€" a "cts" cuando baja de un € el importe.
+
 			bool importeEsNum = double.TryParse(txtEuros.Text, out double importeIntroducido);
 
 			if ((importeEsNum) && (importeIntroducido > 0))
 				{
 
+				// La cantidad introducida en el TextBox se irá pasando por referencia y se irá descontando cada vez que se obtengan las unidades
+				// A continuación se envían los resultados obtenidos a la función 'ImprimirResultado()' donde se van concatenando
+				unidades = CalcularUnidades(ref importeIntroducido, 500);
+				ImprimirResultado(unidades, 500, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 200);
+				ImprimirResultado(unidades, 100, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 100);
+				ImprimirResultado(unidades, 100, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 50);
+				ImprimirResultado(unidades, 50, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 20);
+				ImprimirResultado(unidades, 20, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 10);
+				ImprimirResultado(unidades, 10, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 5);
+				ImprimirResultado(unidades, 5, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 2);
+				ImprimirResultado(unidades, 2, centimosEuros);
+
+				unidades = CalcularUnidades(ref importeIntroducido, 1);
+				ImprimirResultado(unidades, 1, centimosEuros);
+
+				// Cuando el importeIntroducido desciende de la cantidad correspondiente a un euro la variable 'centimosEuros' pasa a valer "cts"
+				centimosEuros = "cts";
+
+				// Para evitar la desviación de los decimales en la cantidad a descontar
+				// la almaceno en la variable 'centimos' = 'importeIntroducido * 101... PARA QUE DEVUELVA EL CÉNTIMO!!!!!!!
+				// A partir de aquí se llama a la función CalcularCentimos() para obtener las unidades de céntimos
+
+
+				centimos = importeIntroducido * 101;
+
+				unidades = CalcularCentimos(ref centimos, 50);
+				ImprimirResultado(unidades, 50, centimosEuros);
+
+				unidades = CalcularCentimos(ref centimos, 20);
+				ImprimirResultado(unidades, 20, centimosEuros);
+
+				unidades = CalcularCentimos(ref centimos, 10);
+				ImprimirResultado(unidades, 10, centimosEuros);
+
+				unidades = CalcularCentimos(ref centimos, 5);
+				ImprimirResultado(unidades, 5, centimosEuros);
+
+				unidades = CalcularCentimos(ref centimos, 2);
+				ImprimirResultado(unidades, 2, centimosEuros);
+
+				unidades = CalcularCentimos(ref centimos, 1);
+				ImprimirResultado(unidades, 1, centimosEuros);
+				
 				}
 			else
 				{
 				lblResultado.Text = "Introduzca una cantidad positiva.";
 				}
 
-
+		
 
 			}
+
+		public int CalcularUnidades(ref double centimos, double fraccion)
+			{
+				double unidades = (centimos / fraccion);
+				unidades = (int)unidades;
+				centimos -= (fraccion * (double)unidades);
+				return ((int)unidades);
+			}
+
+		public int CalcularCentimos(ref double centimos, double fraccion)
+			{
+			double unidades = (centimos / fraccion);
+			unidades = (int)unidades;
+			centimos -= (fraccion * (double)unidades);
+			return ((int)unidades);
+			}
+
+
+
+
+		public void ImprimirResultado(int unidades, int fraccion, string centimosEuros)
+			{
+			string tipoCambio = "";
+			string plural = "";
+
+			// Un gran bloque condicional discrimina si se trata de euros o céntimos para distinguir, en el caso de los euros, entre monedas o billetes
+			if (centimosEuros != "cts")
+				{ 
+					if (fraccion > 2)
+					{
+						tipoCambio = "billete";
+					}
+					else
+					{
+						tipoCambio = "moneda";
+					}
+				}
+
+				if (unidades > 1)
+					{
+					plural = "s";
+					}
+				else
+					{
+					plural = "";
+					}
+
+			if (unidades > 0) { 
+				lblResultado.Text += $"{unidades} {tipoCambio}{plural} de {fraccion} {centimosEuros}\n";
+				}
+			}
+
+
+
 		}
 	}
--- Tema_04/Tema_04_Ejercicio_15/Tests_Tema_4_Ej_15/Tests_Tema_4_Ej_15.cs
+++ Tema_04/Tema_04_Ejercicio_15/Tests_Tema_4_Ej_15/Tests_Tema_4_Ej_15.cs
@@ -10,14 +10,35 @@ namespace Tests_Tema_4_Ej_15
 		{
 		[TestMethod]
 		[DataRow(5000,500,10)]
-		public void TestMethod1()
+		[DataRow(6, 2, 3)]
+		[DataRow(1, 1, 1)]
+		public void TestMethodCambioImporteEuros(double importe, double fraccion, int expected)
 			{
 
 			// Act
-
+			Prog_Tema_4_Ej_15 objetoPrograma = new Prog_Tema_4_Ej_15();
+			int actual = objetoPrograma.CalcularUnidades(ref importe, fraccion);
 			// Assert
-
-
+			Assert.AreEqual(expected, actual);
 			}
+
+		[TestMethod]
+		[DataRow(70, 50, 1)]
+		[DataRow(23, 10, 2)]
+		[DataRow(1, 1, 1)]
+		[DataRow(3, 2, 1)]
+		public void TestMethodCambioImporteCentimos(double importe, double fraccion, int expected)
+			{
+
+			// Act
+			Prog_Tema_4_Ej_15 objetoPrograma = new Prog_Tema_4_Ej_15();
+			int actual = objetoPrograma.CalcularCentimos(ref importe, fraccion);
+			// Assert
+			Assert.AreEqual(expected, actual);
+			}
+
+
+
+
 		}
 	}
